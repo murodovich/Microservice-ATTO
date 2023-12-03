5a58bde34598c064739c4799f805697344926400
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-03 23:49:53 +0500

    DependecyInJection

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/DependecyInjection.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace University.Application
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddAplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
