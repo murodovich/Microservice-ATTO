64b9890158a9f91b2802f0af73988e000a3527e0
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-03 23:49:26 +0500

    CreateStudentCommandHandler

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/Students/Handlers/CreateStudentCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using University.Application.Absreactions;
+using University.Application.UseCases.Students.Commands;
+using University.Domain.Entities.Models;
+
+namespace University.Application.UseCases.Students.Handlers
+{
+    public class CreateStudentCommandHandler : AsyncRequestHandler<CreateStudentCommand>
+    {
+        private readonly IUniversityDBContext _dbContext;
+
+        public CreateStudentCommandHandler(IUniversityDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        protected override async Task Handle(CreateStudentCommand request, CancellationToken cancellationToken)
+        {
+            var student = new Student()
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Phone = request.Phone,
+                Age = request.Age,
+               
+                Country = request.Country,
+                City = request.City
+            };
+
+            await _dbContext.students.AddAsync(student);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
