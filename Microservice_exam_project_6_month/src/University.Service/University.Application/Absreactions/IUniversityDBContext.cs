ad7a6bfa3737f9838e949bd49561f4906a860622
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-03 23:49:11 +0500

    IUniversityDBcontext

--- Microservice_exam_project_6_month/src/University.Service/University.Application/Absreactions/IUniversityDBContext.cs
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/Absreactions/IUniversityDBContext.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using University.Domain.Entities.Entities;
+using University.Domain.Entities.Models;
 
 namespace University.Application.Absreactions
 {
@@ -7,9 +7,14 @@ namespace University.Application.Absreactions
     {
         public DbSet<Student> students { get; set; }
         public DbSet<Course> courses { get; set; }
-        public DbSet<CourseEnrollment> enrollments { get; set; }
-        public DbSet<Department> departments { get; set; }  
-        public DbSet<Instructor> instructors { get; set; }
+        public DbSet<Teacher> teachers { get; set; }
+        public DbSet<TaskGrade> tasks { get; set; }
+        public DbSet<Subject> subjects { get; set; }
+        public DbSet<Lesson> lessons { get; set; }
+        public DbSet<Group> groups { get; set; }
+        public DbSet<Dedline> dededlines { get; set; }
+        public DbSet<CourseGroup> coursegroups { get; set; }
+        public DbSet<Attendance> attendances { get; set; }
         public Task<int> SaveChangesAsync(CancellationToken cancellationToken);
     }
 }
