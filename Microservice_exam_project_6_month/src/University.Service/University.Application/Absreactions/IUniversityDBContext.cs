1845276b4512e215edfab5ebd0e169abaf7f94a9
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-03 17:38:39 +0500

    IUniverSityDBContext

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/Absreactions/IUniversityDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using University.Domain.Entities.Entities;
+
+namespace University.Application.Absreactions
+{
+    public interface IUniversityDBContext
+    {
+        public DbSet<Student> students { get; set; }
+        public DbSet<Course> courses { get; set; }
+        public DbSet<CourseEnrollment> enrollments { get; set; }
+        public DbSet<Department> departments { get; set; }  
+        public DbSet<Instructor> instructors { get; set; }
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
