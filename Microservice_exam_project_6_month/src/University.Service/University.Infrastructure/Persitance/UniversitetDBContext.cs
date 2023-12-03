81fdbbd98e872c8b8b985d536935fca30e57f8f5
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-03 23:51:24 +0500

    Context'

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Infrastructure/Persitance/UniversitetDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using University.Application.Absreactions;
+using University.Domain.Entities.Models;
+
+namespace University.Infrastructure.Persitance
+{
+    public class UniversitetDBContext : DbContext, IUniversityDBContext
+    {
+        public UniversitetDBContext(DbContextOptions<UniversitetDBContext> options) : base(options) { }
+
+        public DbSet<Student> students { get; set; }
+        public DbSet<Course> courses { get; set; }
+        public DbSet<Teacher> teachers { get; set; }
+        public DbSet<TaskGrade> tasks { get; set; }
+        public DbSet<Subject> subjects { get; set; }
+        public DbSet<Lesson> lessons { get; set; }
+        public DbSet<Group> groups { get; set; }
+        public DbSet<Dedline> dededlines { get; set; }
+        public DbSet<CourseGroup> coursegroups { get; set; }
+        public DbSet<Attendance> attendances { get; set; }
+
+    }
+}
