1ffa318bd65fb8a81bd099748d16a8ffc8f5062b
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 17:53:22 +0500

    Groups Controller

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Api/Controllers/Groups/GroupController.cs
@@ -0,0 +1,70 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using University.Api.Dtos;
+using University.Application.UseCases.Groups.Commands;
+using University.Application.UseCases.Groups.Queries;
+
+namespace University.Api.Controllers.Groups
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class GroupController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public GroupController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllGroup()
+        {
+            var result = await _mediator.Send(new GetGroupCommand());
+            return Ok(result);
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateGroup(GroupDto groupDto)
+        {
+            var group = new CreateGroupCommand()
+            {
+                Name = groupDto.Name,
+                Lavel = groupDto.Level
+            };
+            await _mediator.Send(group);
+            return Ok("Created Group");
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetByIdGroup(int id)
+        {
+            var group = new GetByIdGroupCommand()
+            {
+                Id = id
+            };
+            var result = await _mediator.Send(group);
+            return Ok(result);
+        }
+        [HttpPut]
+        public async ValueTask<IActionResult> UpdateGroup(int Id, GroupDto groupDto)
+        {
+            var group = new UpdateGroupCommand()
+            {
+                GroupId = Id,
+                Name = groupDto.Name,
+                Lavel = groupDto.Level
+            };
+            await _mediator.Send(group);
+            return Ok("Updated group");
+        }
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteGroup(int id)
+        {
+            var group = new DeleteGroupCommand()
+            {
+                Id = id
+            };
+            await _mediator.Send(group);
+            return Ok("Deleted group");
+        }
+    }
+}
