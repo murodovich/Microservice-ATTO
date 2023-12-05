97aac749ffe3338e6ae51c1892e62404dd8be753
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 17:55:18 +0500

    Fileexception

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Domain/Exceptions/FileExceptions/FileNotValid.cs
@@ -0,0 +1,12 @@
+﻿namespace University.Domain.Exceptions.FileExceptions
+{
+    public class FileNotValid :  Exception
+    {
+       
+        public string message { get; set; }
+        public FileNotValid() 
+        {
+            message = "File not Valid!";
+        }
+    }
+}
