087c6daf718fc55fa6ec2c124af7b90f5bdc6072
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 16:09:26 +0500

    Dedline

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Domain/Exceptions/Dedlineis/DedlineNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace University.Domain.Exceptions.Dedlineis
+{
+    public class DedlineNotFoundException : NotFoundException 
+    {
+        public DedlineNotFoundException()
+        {
+            this.TitleMessage = "Dedline not Found !";
+        }
+    }
+}
