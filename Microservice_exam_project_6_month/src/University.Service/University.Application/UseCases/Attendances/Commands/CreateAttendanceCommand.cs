d752a3d68488c10be3e02daf2609db5d9202b4eb
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 16:04:09 +0500

    createAttendancecommand

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/Attendances/Commands/CreateAttendanceCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace University.Application.UseCases.Attendances.Commands
+{
+    public class CreateAttendanceCommand : IRequest<bool>
+    {
+        public int LessonId { get; set; }
+        public int StudentId { get; set; }
+        public bool Attendances { get; set; }
+    }
+}
