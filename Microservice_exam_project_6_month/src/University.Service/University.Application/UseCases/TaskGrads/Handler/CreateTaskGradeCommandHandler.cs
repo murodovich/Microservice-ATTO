a1b8db186465446ca42612f931292692abbd8401
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 18:33:21 +0500

    Create Update Handler

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/TaskGrads/Handler/CreateTaskGradeCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using University.Application.Absreactions;
+using University.Application.Interfaces;
+using University.Application.UseCases.TaskGrads.Commands;
+using University.Domain.Models;
+
+namespace University.Application.UseCases.TaskGrads.Handler
+{
+    public class CreateTaskGradeCommandHandler : IRequestHandler<CreateTaskGradeCommand, bool>
+    {
+        private readonly IUniversityDBContext _dbContext;
+        private readonly IFileService _fileService;
+
+        public CreateTaskGradeCommandHandler(IUniversityDBContext dbContext, IFileService fileService)
+        {
+            _dbContext = dbContext;
+            _fileService = fileService;
+        }
+
+        public async Task<bool> Handle(CreateTaskGradeCommand request, CancellationToken cancellationToken)
+        {
+            string filepage = await _fileService.UploadImageAsync(request.FilePath);
+            if (string.IsNullOrEmpty(filepage)) throw new FileNotFoundException(nameof(filepage));
+
+            var result = new TaskGrade()
+            {
+                FilePath = filepage,
+                DedlineId = request.DedlineId,
+                StudentId = request.StudentId,
+            };
+            await _dbContext.tasks.AddAsync(result);
+            var res = await _dbContext.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
+    }
+}
---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/TaskGrads/Handler/UpdateTaskGradeCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using University.Application.Absreactions;
+using University.Application.Interfaces;
+using University.Application.UseCases.TaskGrads.Commands;
+using University.Domain.Exceptions.TaskGrade;
+
+namespace University.Application.UseCases.TaskGrads.Handler
+{
+    public class UpdateTaskGradeCommandHandler : IRequestHandler<UpdateTaskGradeCommand, bool>
+    {
+        private readonly IUniversityDBContext _dbContext;
+        private readonly IFileService _fileService;
+
+        public UpdateTaskGradeCommandHandler(IUniversityDBContext dbContext, IFileService fileService)
+        {
+            _dbContext = dbContext;
+            _fileService = fileService;
+        }
+
+        public async Task<bool> Handle(UpdateTaskGradeCommand request, CancellationToken cancellationToken)
+        {
+            string filepage = await _fileService.UploadImageAsync(request.FilePath);
+            if (string.IsNullOrEmpty(filepage)) throw new FileNotFoundException(nameof(filepage));
+
+            var res = await _dbContext.tasks.FirstOrDefaultAsync(x => x.TaskGradeId == request.Id);
+            if (res != null) throw new TaskGradeNotFoundException();
+
+            res.StudentId = request.StudentId;
+            res.DedlineId = request.DedlineId;
+            res.FilePath = filepage;
+
+            _dbContext.tasks.Update(res);
+            var result = await _dbContext.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
+    }
+}
