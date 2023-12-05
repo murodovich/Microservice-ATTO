b8a1a1619f10e69b1e5cb048951e5d723751473f
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 16:09:40 +0500

    Lesson

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Domain/Exceptions/Lessons/LessonsNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace University.Domain.Exceptions.Lessons
+{
+    public class LessonsNotFoundException : NotFoundException
+    {
+        public LessonsNotFoundException()
+        {
+            this.TitleMessage = "Lesson not Found !";
+        }
+    }
+}
