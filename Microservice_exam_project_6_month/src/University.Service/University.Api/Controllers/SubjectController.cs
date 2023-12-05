2daaa38d777ffc1a162a764eb080d2f70e84db5c
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 16:03:16 +0500

    SubjectController

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Api/Controllers/SubjectController.cs
@@ -0,0 +1,66 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using University.Application.UseCases.Subjects.Commands;
+using University.Application.UseCases.Subjects.Queries;
+
+namespace University.Api.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class SubjectController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public SubjectController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllSubject()
+        {
+            var res = await _mediator.Send(new GetAllSubjectQuery());
+            return Ok(res);
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateSubject(CreateSubjectCommand createSubject) 
+        {
+            var res = new CreateSubjectCommand()
+            {
+                SubjectName = createSubject.SubjectName,
+            };
+            await _mediator.Send(res);
+            return Ok("Created Subject");
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetByIdSubject(int Id) 
+        {
+            var res = new GetByIdSubjectQuery()
+            {
+                Id = Id
+            };
+            var result = await _mediator.Send(res);
+            return Ok(result);
+        }
+        [HttpPut]
+        public async ValueTask<IActionResult> Update(UpdateSubjectCommand updateSubject)
+        {
+            var res = new UpdateSubjectCommand()
+            {
+                SubjectId = updateSubject.SubjectId,               
+                SubjectName = updateSubject.SubjectName,
+            };
+            await _mediator.Send(res);
+            return Ok("Updated");
+        }
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteSubject(int Id)
+        {
+            var res = new DeleteSubjectCommand()
+            {
+                Id = Id
+            };
+            await _mediator.Send(res);
+            return Ok("Deleted");
+        }
+    }
+}
