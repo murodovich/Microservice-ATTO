88d54f86d75579de89e2cc6a178137e59e9eb973
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 18:33:12 +0500

    UpdateTaskGrade

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/TaskGrads/Commands/UpdateTaskGradeCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+
+namespace University.Application.UseCases.TaskGrads.Commands
+{
+    public class UpdateTaskGradeCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+        public int DedlineId { get; set; }
+        public int StudentId { get; set; }
+        public IFormFile FilePath { get; set; }
+    }
+}
