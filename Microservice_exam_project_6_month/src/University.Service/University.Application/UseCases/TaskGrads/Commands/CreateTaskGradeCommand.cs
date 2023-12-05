2c4feb700268c56eb843309e6fd2cb78242284ca
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 16:07:37 +0500

    command

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/TaskGrads/Commands/CreateTaskGradeCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace University.Application.UseCases.TaskGrads.Commands
+{
+    public class CreateTaskGradeCommand : IRequest<bool>
+    {
+        public int DedlineId { get; set; }
+        public int StudentId { get; set; }
+        public string FilePath { get; set; }
+    }
+}
