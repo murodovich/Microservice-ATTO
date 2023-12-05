a54e6009d8e2630f2c80b847f6f59ce17ee16175
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 17:54:27 +0500

    FIleSevice

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/Interfaces/IFileService.cs
@@ -0,0 +1,12 @@
+﻿
+using Microsoft.AspNetCore.Http;
+
+namespace University.Application.Interfaces
+{
+    public interface IFileService 
+    {
+        ValueTask<string> UploadImageAsync(IFormFile file);
+        ValueTask<bool> DeleteImageAsync(string file);
+        ValueTask<byte[]> GetImageAsync(string path);
+    }
+}
