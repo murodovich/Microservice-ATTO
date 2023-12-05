e5415fab4ac5ed7c2e6facffb0414842f7061a15
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 18:59:00 +0500

    DeleteTaskController

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/TaskGrads/Commands/DeleteTaskGradeCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace University.Application.UseCases.TaskGrads.Commands
+{
+    public class DeleteTaskGradeCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+}
