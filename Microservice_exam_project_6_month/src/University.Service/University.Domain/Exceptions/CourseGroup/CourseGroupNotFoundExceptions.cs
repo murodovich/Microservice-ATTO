cd2945dfd278dfa23561affb298a6349c85ae9ef
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 16:09:05 +0500

    Course

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Domain/Exceptions/CourseGroup/CourseGroupNotFoundExceptions.cs
@@ -0,0 +1,10 @@
+﻿namespace University.Domain.Exceptions.CourseGroup
+{
+    public class CourseGroupNotFoundExceptions : NotFoundException
+    {
+        public CourseGroupNotFoundExceptions()
+        {
+            this.TitleMessage = "CourseGroup not Found!";
+        }
+    }
+}
