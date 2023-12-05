3e9d9041de9aa5abfbeb299579515a93bc9f5fa3
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 16:07:21 +0500

    Query

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/Subjects/Queries/GetAllSubjectQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using University.Domain.Models;
+
+namespace University.Application.UseCases.Subjects.Queries
+{
+    public class GetAllSubjectQuery : IRequest<List<Subject>>
+    {
+    }
+}
---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/Subjects/Queries/GetByIdSubjectQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using University.Domain.Models;
+
+namespace University.Application.UseCases.Subjects.Queries
+{
+    public class GetByIdSubjectQuery : IRequest<Subject>
+    {
+        public int Id { get; set; }
+
+    }
+}
