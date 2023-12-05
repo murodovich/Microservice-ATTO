46ea80bacd34fa391644f31d672936debe018998
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 17:54:56 +0500

    GetQuery

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/Dedlineis/Queries/GetAllDedlineQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using University.Domain.Models;
+
+namespace University.Application.UseCases.Dedlineis.Queries
+{
+    public class GetAllDedlineQuery : IRequest<List<Dedline>>
+    {
+    }
+}
---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/Dedlineis/Queries/GetByIdDedlineQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using University.Domain.Models;
+
+namespace University.Application.UseCases.Dedlineis.Queries
+{
+    public class GetByIdDedlineQuery : IRequest<Dedline>
+    {
+        public int Id { get; set; }
+
+    }
+}
