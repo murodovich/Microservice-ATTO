353d93e538f6fbfbef81b3022b6ec6a555576f0a
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 16:04:50 +0500

    GetByIdAttendanceQuery

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/Attendances/Queries/GetByIdAttendanceQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using University.Domain.Models;
+
+namespace University.Application.UseCases.Attendances.Queries
+{
+    public class GetByIdAttendanceQuery : IRequest<Attendance>
+    {
+        public int Id { get; set; }
+
+    }
+}
