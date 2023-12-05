62cc57dbf1c0212a0a50e83e4d59ee0153ec09fd
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 17:54:18 +0500

    FileService

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/FileServices/FileService.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using University.Application.Common.Halpers;
+using University.Application.Interfaces;
+
+namespace University.Application.FileServices
+{
+    public class FileService : IFileService
+    {
+        private readonly string MEDIA = "media";
+        private readonly string IMAGES = "images";
+        private readonly string ROOTPATH;
+        public FileService()
+        {
+            ROOTPATH = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+        }
+
+        public async ValueTask<bool> DeleteImageAsync(string file)
+        {
+            string path = Path.Combine(ROOTPATH, file);
+
+            if (File.Exists(path))
+            {
+                await Task.Run(() =>
+                {
+                    File.Delete(path);
+                });
+                return true;
+            }
+            return false;
+        }
+
+        public async ValueTask<byte[]> GetImageAsync(string fileName)
+        {
+            string path = Path.Combine(ROOTPATH, fileName);
+            byte[] imageBytes = await File.ReadAllBytesAsync(path);
+            return imageBytes;
+        }
+
+        public async ValueTask<string> UploadImageAsync(IFormFile file)
+        {
+            string newImageName = MediaHelper.MakeImageName(file.FileName.ToLower());
+            string subPath = Path.Combine(MEDIA, IMAGES, newImageName);
+            string path = Path.Combine(ROOTPATH, subPath);
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+                return subPath;
+            }
+        }
+    }
+}
