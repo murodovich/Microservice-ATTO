4a41b9c4fade67dc6d7b808f6fc1c678d5496843
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 16:10:40 +0500

    Depen

--- Microservice_exam_project_6_month/src/University.Service/University.Infrastructure/DependecyInjection.cs
+++ Microservice_exam_project_6_month/src/University.Service/University.Infrastructure/DependecyInjection.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using University.Application.Absreactions;
