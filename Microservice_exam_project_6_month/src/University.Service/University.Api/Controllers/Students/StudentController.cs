5147195114379fc618ccf2ac1ee43d0171e7216c
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 17:53:36 +0500

    Student Controller

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Api/Controllers/Students/StudentController.cs
@@ -0,0 +1,95 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using University.Api.Dtos;
+using University.Application.UseCases.Students.Commands;
+using University.Application.UseCases.Students.Queries;
+
+namespace University.Api.Controllers.Students
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public StudentController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateStudent(StudentDto studentDto)
+        {
+            var command = new CreateStudentCommand()
+            {
+                FirstName = studentDto.FirstName,
+                LastName = studentDto.LastName,
+                Email = studentDto.Email,
+                Phone = studentDto.Phone,
+                Age = studentDto.Age,
+                Country = studentDto.Country,
+                City = studentDto.City,
+                Gender = studentDto.Gender,
+                GroupId = studentDto.GroupId,
+                Role = studentDto.Role,
+                CreatedAt = studentDto.CreatedAt,
+                UpdatedAt = studentDto.UpdatedAt,
+            };
+
+            await _mediator.Send(command);
+            return Ok("Created");
+
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetStudent()
+        {
+            var student = await _mediator.Send(new GetStudentCommand());
+            return Ok(student);
+        }
+
+        [HttpPut]
+        public async ValueTask<IActionResult> UpdateStudent(int id, StudentDto studentDto)
+        {
+            var command = new UpdateStudentCommand()
+            {
+                StudentId = id,
+                FirstName = studentDto.FirstName,
+                LastName = studentDto.LastName,
+                Email = studentDto.Email,
+                Phone = studentDto.Phone,
+                Age = studentDto.Age,
+                Country = studentDto.Country,
+                City = studentDto.City,
+                Gender = studentDto.Gender,
+                GroupId = studentDto.GroupId,
+                Role = studentDto.Role,
+                CreatedAt = studentDto.CreatedAt,
+                UpdatedAt = studentDto.UpdatedAt,
+            };
+
+            await _mediator.Send(command);
+            return Ok("Updated");
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetByIdStudent(int id)
+        {
+            var command = new GetByIdStudentCommand()
+            {
+                Id = id
+            };
+            var student = await _mediator.Send(command);
+            return Ok(student);
+        }
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteStudent(int id)
+        {
+            var command = new DeleteStudentCommand()
+            {
+                Id = id
+            };
+            await _mediator.Send(command);
+            return Ok("Deleted");
+        }
+
+    }
+}
