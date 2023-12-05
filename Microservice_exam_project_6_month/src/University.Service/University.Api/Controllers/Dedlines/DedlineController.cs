53b26ad9a1330785f285166f936eca288778b243
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 17:53:15 +0500

    Dedline Controller

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Api/Controllers/Dedlines/DedlineController.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using University.Application.UseCases.Dedlineis.Commands;
+using University.Application.UseCases.Dedlineis.Queries;
+
+namespace University.Api.Controllers.Dedlines
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class DedlineController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public DedlineController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateDedline([FromForm] CreateDedlineCommand command)
+        {
+
+            await _mediator.Send(command);
+            return Ok("Created");
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllDedline()
+        {
+            var res = await _mediator.Send(new GetAllDedlineQuery());
+            return Ok(res);
+        }
+        [HttpPut]
+        public async ValueTask<IActionResult> UpdateDedline(UpdateDedlineComman updateDedlineComman)
+        {
+            await _mediator.Send(updateDedlineComman);
+            return Ok("Update Dedline");
+        }
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteDedline(int id)
+        {
+            await _mediator.Send(new DeleteDedlineCommand { Id = id });
+            return Ok("Deleted Dedline");
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetByIdDedline(int id)
+        {
+            var res = await _mediator.Send(new GetByIdDedlineQuery { Id = id });
+            return Ok(res);
+        }
+    }
+}
