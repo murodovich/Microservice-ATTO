c583af83bc34e0c1a46d090b9aaea36750e22b08
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 16:09:33 +0500

    Groups

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Domain/Exceptions/Groups/GroupsNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace University.Domain.Exceptions.Groups
+{
+    public class GroupsNotFoundException : NotFoundException
+    {
+        public GroupsNotFoundException()
+        {
+            this.TitleMessage = "Group not Found !";
+        }
+    }
+}
