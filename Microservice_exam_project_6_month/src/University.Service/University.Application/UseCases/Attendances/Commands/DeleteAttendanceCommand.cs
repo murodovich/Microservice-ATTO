625fc8ab77e632a2ebd878b37d768bd4b422459a
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 16:04:19 +0500

    Attendance

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/Attendances/Commands/DeleteAttendanceCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace University.Application.UseCases.Attendances.Commands
+{
+    public class DeleteAttendanceCommand :  IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/Attendances/Commands/UpdateAttendanceCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace University.Application.UseCases.Attendances.Commands
+{
+    public class UpdateAttendanceCommand : IRequest<bool>
+    {
+        public int AttendanceId { get; set; }
+        public int LessonId { get; set; }
+        public int StudentId { get; set; }
+        public bool Attendances { get; set; }
+    }
+}
