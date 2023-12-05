fa72555803b9b7591458b44e9e4822a68618a925
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 16:10:03 +0500

    taskGrade

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Domain/Exceptions/TaskGrade/TaskGradeNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace University.Domain.Exceptions.TaskGrade
+{
+    public class TaskGradeNotFoundException : NotFoundException
+    {
+        public TaskGradeNotFoundException()
+        {
+            TitleMessage = "TaskGrade not Found !";
+        }
+    }
+}
