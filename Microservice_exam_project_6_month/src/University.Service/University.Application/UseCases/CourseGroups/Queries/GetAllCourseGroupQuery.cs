3a30ccb60dfac0a20aad732f1a632877ded2055b
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 16:05:17 +0500

    Get

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/CourseGroups/Queries/GetAllCourseGroupQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using University.Domain.Models;
+
+namespace University.Application.UseCases.CourseGroups.Queries
+{
+    public class GetAllCourseGroupQuery :IRequest<List<CourseGroup>>
+    {
+    }
+}
---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/CourseGroups/Queries/GetByIdCourseGroupQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using University.Domain.Models;
+
+namespace University.Application.UseCases.CourseGroups.Queries
+{
+    public class GetByIdCourseGroupQuery : IRequest<CourseGroup>
+    {
+        public int Id { get; set; }
+    }
+}
