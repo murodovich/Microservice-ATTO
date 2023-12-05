cab2169f782d997e892fa194f8f15e11e20c2e43
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 17:55:08 +0500

    Application

--- Microservice_exam_project_6_month/src/University.Service/University.Application/DependecyInjection.cs
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/DependecyInjection.cs
@@ -1,6 +1,8 @@
 ﻿using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
+using University.Application.FileServices;
+using University.Application.Interfaces;
 
 namespace University.Application
 {
@@ -9,6 +11,7 @@ namespace University.Application
         public static IServiceCollection AddAplication(this IServiceCollection services)
         {
             services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddScoped<IFileService, FileService>();
             return services;
         }
 
