c5bd5b8b18459ccaba4ae65d62ea43408a185b46
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 16:08:54 +0500

    Attendance

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Domain/Exceptions/Attendanceis/AttendanceNotFoundException.cs
@@ -0,0 +1,11 @@
+﻿namespace University.Domain.Exceptions.Attendanceis
+{
+    public class AttendanceNotFoundException : NotFoundException
+    {
+        public AttendanceNotFoundException() 
+        {
+            this.TitleMessage = "Attendance not Found !";
+        }
+
+    }
+}
