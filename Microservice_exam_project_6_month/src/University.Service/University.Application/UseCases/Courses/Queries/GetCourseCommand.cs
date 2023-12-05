4004734b595ef20e42c9acfdb5d5799f74a756db
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 16:05:45 +0500

    Queries

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/Courses/Queries/GetCourseCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using University.Domain.Models;
+
+namespace University.Application.UseCases.Courses.Queries
+{
+    public class GetCourseCommand : IRequest<List<Course>>
+    {
+    }
+}
