2dd740b6660e564c58da03c4bf4aa12d2c52bf31
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 18:32:51 +0500

    sa

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Api/Controllers/TaskGrade/TaskGradeController.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using University.Application.UseCases.TaskGrads.Commands;
+
+namespace University.Api.Controllers.TaskGrade
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class TaskGradeController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TaskGradeController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateTaskGrade([FromForm] CreateTaskGradeCommand command)
+        {
+            var res = await _mediator.Send(command);
+            return Ok("Created");
+        }
+    }
+}
