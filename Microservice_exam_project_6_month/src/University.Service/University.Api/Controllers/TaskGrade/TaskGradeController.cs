f7b620fed2396d4223b65a372db034ff246f01fb
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 18:58:48 +0500

    taskGradeController

--- Microservice_exam_project_6_month/src/University.Service/University.Api/Controllers/TaskGrade/TaskGradeController.cs
+++ Microservice_exam_project_6_month/src/University.Service/University.Api/Controllers/TaskGrade/TaskGradeController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using University.Application.UseCases.TaskGrads.Commands;
+using University.Application.UseCases.TaskGrads.Queries;
 
 namespace University.Api.Controllers.TaskGrade
 {
@@ -20,6 +21,30 @@ namespace University.Api.Controllers.TaskGrade
         {
             var res = await _mediator.Send(command);
             return Ok("Created");
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllTaskGrade()
+        {
+            var res = await _mediator.Send(new GetAllTaskGradeQuery());
+            return Ok(res);
+        }
+        [HttpPut]
+        public async ValueTask<IActionResult> UpdateTaskGrade(UpdateTaskGradeCommand updateTaskGradeCommand)
+        {
+            await _mediator.Send(updateTaskGradeCommand);
+            return Ok("Updated");
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetByIdTaskGrade(int id)
+        {
+            var res = await _mediator.Send(new GetByIdTaskGradeQuery() { Id = id});
+            return Ok(res);
+        }
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteTaskGrade(int id)
+        {
+            await _mediator.Send(new DeleteTaskGradeCommand() { Id = id});
+            return Ok("Deleted");
         }
     }
 }
