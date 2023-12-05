fdb9f8565435c5ed5c5bbd22398616670d0c33f2
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 16:08:22 +0500

    Models

--- Microservice_exam_project_6_month/src/University.Service/University.Domain/Entities/Models/Subject.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace University.Domain.Entities.Models
-{
-    public class Subject
-    {
-        public int SubjectId { get; set; }
-        public string SubjectName { get; set; }
-    }
-}
