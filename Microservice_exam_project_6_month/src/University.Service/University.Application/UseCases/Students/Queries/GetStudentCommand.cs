a825e8c3d6fb93624cec90f8ba32aa4921d4132b
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 16:07:05 +0500

    student

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/Students/Queries/GetStudentCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using University.Domain.Models;
+
+namespace University.Application.UseCases.Students.Queries
+{
+    public class GetStudentCommand : IRequest<List<Student>>
+    {
+    }
+}
