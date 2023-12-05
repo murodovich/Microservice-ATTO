dc6573a6b5fe77501720dbd5b36eebac366d176c
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 16:05:52 +0500

    Crea

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/Dedlineis/Commands/CreateDedlineCommand.cs
@@ -0,0 +1,13 @@
+﻿using MediatR;
+
+namespace University.Application.UseCases.Dedlineis.Commands
+{
+    public class CreateDedlineCommand : IRequest<bool>
+    {
+        public int MaxGrade { get; set; }
+        public DateTime StartDate { get; set; } = DateTime.Now;
+        public DateTime ExpiredDate { get; set; }
+        public string FilePage { get; set; }
+        public int CourseId { get; set; }
+    }
+}
