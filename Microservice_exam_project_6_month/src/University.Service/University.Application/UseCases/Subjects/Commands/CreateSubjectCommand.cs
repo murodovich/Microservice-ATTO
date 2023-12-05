738aff7af73ca1c966acdc5a6f665d4e33754696
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 16:07:10 +0500

    Command

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/Subjects/Commands/CreateSubjectCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace University.Application.UseCases.Subjects.Commands
+{
+    public class CreateSubjectCommand : IRequest<bool>
+    {
+        public string SubjectName { get; set; }
+    }
+}
---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/Subjects/Commands/UpdateSubjectCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace University.Application.UseCases.Subjects.Commands
+{
+    public class UpdateSubjectCommand : IRequest<bool>
+    {
+        public int SubjectId { get; set; }
+        public string SubjectName { get; set; }
+    }
+}
