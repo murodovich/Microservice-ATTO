b4b7a1e6d77e2413ddf47a72d263f4ad45061d12
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 16:06:44 +0500

    Query

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/Lessons/Queries/GetAllLessonQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using University.Domain.Models;
+
+namespace University.Application.UseCases.Lessons.Queries
+{
+    public class GetAllLessonQuery : IRequest<List<Lesson>>
+    {
+    }
+}
---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/Lessons/Queries/GetByIdLessonQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using University.Domain.Models;
+
+namespace University.Application.UseCases.Lessons.Queries
+{
+    public class GetByIdLessonQuery : IRequest<Lesson>
+    {
+        public int Id { get; set; }
+    }
+}
