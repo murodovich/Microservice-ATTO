ce7425594018208c04b57ed8aa606e01415eb0e5
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-05 17:54:10 +0500

    Media Halper

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/Common/Halpers/MediaHelper.cs
@@ -0,0 +1,33 @@
+﻿using University.Domain.Exceptions.FileExceptions;
+
+namespace University.Application.Common.Halpers
+{
+    public class MediaHelper
+    {
+        public static string MakeImageName(string filename)
+        {
+            FileInfo fileInfo = new FileInfo(filename);
+
+            string[] ImageExtension = GetImageExtensions();
+
+            if (ImageExtension.Any(x => x == fileInfo.Extension))
+            {
+                string extension = fileInfo.Extension;
+                string name = "File_" + Guid.NewGuid() + extension;
+                return name;
+            }
+            throw new FileNotValid();
+        }
+
+        public static string[] GetImageExtensions()
+        {
+            return new string[]
+            {
+            ".doc", ".docx",
+            ".txt",
+            ".ppt",
+            ".pptx"
+            };
+        }
+    }
+}
