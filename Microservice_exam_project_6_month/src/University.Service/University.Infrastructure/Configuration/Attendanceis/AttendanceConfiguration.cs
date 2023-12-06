96a1e2bb2cc30e54fa0c6a1a88599cfe08ffb1f5
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:42:04 +0500

    Attendance

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Infrastructure/Configuration/Attendanceis/AttendanceConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using University.Domain.Models;
+
+namespace University.Infrastructure.Configuration.Attendanceis
+{
+    public class AttendanceConfiguration : IEntityTypeConfiguration<Attendance>
+    {
+        public void Configure(EntityTypeBuilder<Attendance> builder)
+        {
+            builder.ToTable("Attendances"); 
+
+            builder.HasKey(a => a.AttendanceId); 
+
+            builder.Property(a => a.LessonId)
+                .IsRequired();
+
+            builder.Property(a => a.StudentId)
+                .IsRequired();
+
+            builder.Property(a => a.Attendances)
+                .IsRequired();
+
+        }
+    }
+}
