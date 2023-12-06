3ea2d08f2a1c9cc5184185b639c9a8c0d751fcb9
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:47:03 +0500

    Transport

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Infrastructure/Migrations/20231206134000_Transport1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Transport.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Transport1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+        }
+    }
+}
