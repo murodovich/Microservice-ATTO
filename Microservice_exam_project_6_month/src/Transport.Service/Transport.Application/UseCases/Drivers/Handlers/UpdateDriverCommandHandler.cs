5f2a63a2e6730d0943e58d373d06d53cc2841aeb
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 01:07:11 +0500

    Driver

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Drivers/Handlers/UpdateDriverCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Transport.Application.Absreactions;
+using Transport.Application.UseCases.Drivers.Commands;
+using Transport.Domain.Exceptions.Drivers;
+
+namespace Transport.Application.UseCases.Drivers.Handlers
+{
+    public class UpdateDriverCommandHandler : IRequestHandler<UpdateDriverCommand, bool>
+    {
+        private readonly ITransportDBContext _dbContext;
+
+        public UpdateDriverCommandHandler(ITransportDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> Handle(UpdateDriverCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.drivers.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (result == null) throw new DriverNotFoundException();
+
+            result.DriveName = request.DriverName;
+            result.LicenseNumber = request.LicenseNumber;
+            result.TransportId = request.TransportId;
+
+            _dbContext.drivers.Update(result);
+
+            var res = await _dbContext.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
+    }
+}
