d8cbbe25762c9f710e87a96f21afb88d7bbab541
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:48:17 +0500

    University

--- Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/Students/Commands/CreateStudentCommand.cs
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/Students/Commands/CreateStudentCommand.cs
@@ -1,4 +1,7 @@
 ﻿using MediatR;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
 using University.Domain.Enums;
 
 namespace University.Application.UseCases.Students.Commands
@@ -7,15 +10,19 @@ namespace University.Application.UseCases.Students.Commands
     {
         public string LastName { get; set; }
         public string FirstName { get; set; }
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")]
         public string Email { get; set; }
+        [RegularExpression(@"^(\+998|\b998)([1-9][0-9]{8})$")]
         public string Phone { get; set; }
+        public string UserName { get; set; }
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$")]
+        public string Password { get; set; }
         public Gender Gender { get; set; }
         public Role Role { get; set; }
         public int Age { get; set; }
         public string Country { get; set; }
         public string City { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
-        public DateTime UpdatedAt { get; set; }
         public int GroupId { get; set; }
     }
 }
