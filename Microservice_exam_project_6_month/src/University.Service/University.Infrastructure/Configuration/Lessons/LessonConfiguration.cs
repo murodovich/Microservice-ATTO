652cf2e5557ad6ffb2be464fcf97c6ca3e7f5977
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:49:09 +0500

    University

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Infrastructure/Configuration/Lessons/LessonConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using University.Domain.Models;
+
+namespace University.Infrastructure.Configuration.Lessons
+{
+    public class LessonConfiguration : IEntityTypeConfiguration<Lesson>
+    {
+        public void Configure(EntityTypeBuilder<Lesson> builder)
+        {
+            builder.ToTable("Lessons"); 
+
+            builder.HasKey(l => l.LessonId); 
+
+            builder.Property(l => l.CourseId)
+                .IsRequired();
+
+            builder.Property(l => l.LessonName)
+                .IsRequired()
+                .HasMaxLength(255); 
+
+            builder.Property(l => l.Date)
+                .IsRequired();
+        }
+    }
+}
