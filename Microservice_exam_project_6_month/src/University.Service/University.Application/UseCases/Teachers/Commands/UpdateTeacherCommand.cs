4f007b6db7da7db8ab27fb2f9ba30dd3b3d26321
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:48:35 +0500

    University

--- Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/Teachers/Commands/UpdateTeacherCommand.cs
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/Teachers/Commands/UpdateTeacherCommand.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using System.ComponentModel.DataAnnotations;
 using University.Domain.Enums;
 
 namespace University.Application.UseCases.Teachers.Commands
@@ -9,6 +10,9 @@ namespace University.Application.UseCases.Teachers.Commands
         public string LastName { get; set; }
         public string FirstName { get; set; }
         public string FatherName { get; set; }
+        public string UserName { get; set; }
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$")]
+        public string Password { get; set; }
         public string Direction { get; set; }
         public Gender Gender { get; set; }
         public Role Role { get; set; }
