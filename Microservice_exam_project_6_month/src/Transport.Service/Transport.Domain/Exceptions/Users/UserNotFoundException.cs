ecf32e10bbc5431dd5385e6167098b1a7acc5e5c
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 20:12:20 +0500

    Users

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Domain/Exceptions/Users/UserNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Transport.Domain.Exceptions.Users
+{
+    public class UserNotFoundException : NotFoundException
+    {
+        public UserNotFoundException() 
+        {
+            this.TitleMessage = "User Not Found!";
+        }
+    }
+}
