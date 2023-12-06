75b34cc6e4a79d8e3df3997dd92b9823bd0a2e71
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 01:07:32 +0500

    Transport

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Transports/Commands/CreateTransportCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Transport.Application.UseCases.Transports.Commands
+{
+    public class CreateTransportCommand : IRequest<bool>
+    {
+        public string TransportName { get; set; }
+        public string TransportType { get; set; }
+        public int Capacity { get; set; }
+    }
+}
