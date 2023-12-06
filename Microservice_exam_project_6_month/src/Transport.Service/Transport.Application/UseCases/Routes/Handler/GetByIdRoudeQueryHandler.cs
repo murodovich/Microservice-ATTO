c528e3fe5101692e490aff47d31bfc95f418977b
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 20:55:34 +0500

    GetByIdRoute

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Routes/Handler/GetByIdRoudeQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Transport.Application.Absreactions;
+using Transport.Application.UseCases.Routes.Queries;
+using Transport.Domain.Entities.Routeis;
+using Transport.Domain.Exceptions.Routes;
+
+namespace Transport.Application.UseCases.Routes.Handler
+{
+    public class GetByIdRoudeQueryHandler : IRequestHandler<GetByIdRouteQuery, Route>
+    {
+        private readonly ITransportDBContext _dbContext;
+
+        public GetByIdRoudeQueryHandler(ITransportDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Route> Handle(GetByIdRouteQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.routes.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (result == null) throw new RouteNotFoundException();
+            return result;
+
+        }
+    }
+}
