9d9ce3029584a724c7c9a1f56ecd628ff2e66e76
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:44:25 +0500

    CreateUserCommand

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Users/Commonds/CreateUserCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using Transport.Domain.Enums;
+
+namespace Transport.Application.UseCases.Users.Commonds
+{
+    public class CreateUserCommand : IRequest<bool>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
