26818bf6f6a654ff616e509e8a17b2b33d7bf2b2
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 03:08:49 +0500

    User

--- Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Users/Commonds/CreateUserCommand.cs
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Users/Commonds/CreateUserCommand.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using System.ComponentModel.DataAnnotations;
 using Transport.Domain.Enums;
 
 namespace Transport.Application.UseCases.Users.Commonds
@@ -8,9 +9,12 @@ namespace Transport.Application.UseCases.Users.Commonds
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string UserName { get; set; }
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$")]
         public string Email { get; set; }
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$")]
         public string Password { get; set; }
         public Role Role { get; set; }
+        [RegularExpression(@"^(\+998|\b998)([1-9][0-9]{8})$")]
         public string PhoneNumber { get; set; }
         public DateTime CreatedAt { get; set; }
     }
