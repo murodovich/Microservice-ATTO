cd93c8be9fcc84af348fa4c6335c356ed29a328d
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:48:21 +0500

    University

--- Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/Students/Commands/UpdateStudentCommand.cs
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/UseCases/Students/Commands/UpdateStudentCommand.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using System.ComponentModel.DataAnnotations;
 using University.Domain.Enums;
 
 namespace University.Application.UseCases.Students.Commands
@@ -9,13 +10,17 @@ namespace University.Application.UseCases.Students.Commands
         public string LastName { get; set; }
         public string FirstName { get; set; }
         public string Email { get; set; }
+        public string UserName { get; set; }
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$")]
+        public string Password { get; set; }
+        [RegularExpression(@"^(\+998|\b998)([1-9][0-9]{8})$")]
+
         public string Phone { get; set; }
         public Gender Gender { get; set; }
         public Role Role { get; set; }
         public int Age { get; set; }
         public string Country { get; set; }
         public string City { get; set; }
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
         public DateTime UpdatedAt { get; set; }
         public int GroupId { get; set; }
     }
