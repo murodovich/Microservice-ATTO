1cedc01a48bfed20d65f49f81967fe9e62469a30
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 01:37:21 +0500

    DeleteTransport

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Transports/Handler/DeleteTransportCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Transport.Application.Absreactions;
+using Transport.Application.UseCases.Transports.Commands;
+using Transport.Domain.Exceptions.Transports;
+
+namespace Transport.Application.UseCases.Transports.Handler
+{
+    public class DeleteTransportCommandHandler : IRequestHandler<DeleteTransportCommand, bool>
+    {
+        private readonly ITransportDBContext _dbContext;
+
+        public DeleteTransportCommandHandler(ITransportDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> Handle(DeleteTransportCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.transports.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (result == null) throw new TransportNotFoundException();
+
+            _dbContext.transports.Remove(result);   
+
+            var res = await _dbContext.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
+    }
+}
