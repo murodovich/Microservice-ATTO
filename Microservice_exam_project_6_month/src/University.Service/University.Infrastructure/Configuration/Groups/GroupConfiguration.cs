6f66ac63addf345a50452d98fd38f2914d1cc713
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:48:50 +0500

    University

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Infrastructure/Configuration/Groups/GroupConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using University.Domain.Models;
+
+namespace University.Infrastructure.Configuration.Groups
+{
+    public class GroupConfiguration : IEntityTypeConfiguration<Group>
+    {
+        public void Configure(EntityTypeBuilder<Group> builder)
+        {
+            builder.ToTable("Groups"); 
+
+            builder.HasKey(g => g.GroupId); 
+
+            builder.Property(g => g.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.Property(g => g.Level)
+                .IsRequired();
+
+            builder.HasMany(g => g.Courses)
+                .WithOne(cg => cg.Group)
+                .HasForeignKey(cg => cg.GroupId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
