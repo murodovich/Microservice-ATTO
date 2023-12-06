b6e8186791a6d4275a93e32e2e5aa4a6f49dc3ce
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 02:24:35 +0500

    Scheldule

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Scheldules/Handler/UpdateSchelduleCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Transport.Application.Absreactions;
+using Transport.Application.UseCases.Scheldules.Commands;
+using Transport.Domain.Exceptions.Scheldules;
+
+namespace Transport.Application.UseCases.Scheldules.Handler
+{
+    public class UpdateSchelduleCommandHandler : IRequestHandler<UpdateSchelduleCommand, bool>
+    {
+        private readonly ITransportDBContext _dbContext;
+
+        public UpdateSchelduleCommandHandler(ITransportDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> Handle(UpdateSchelduleCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.schedules.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (result == null) throw new SchelduleNotFoundException();
+
+            result.DepartureTime = request.DepartureTime;
+            result.TransportId = request.TransportId;
+            result.RouteId = request.RouteId;
+
+            _dbContext.schedules.Update(result);
+
+            var res = await _dbContext.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
+    }
+}
