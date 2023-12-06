df081fc45810ec2e33b6b9365a9c35972abc1ae5
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:44:34 +0500

    DeleteUserCommand

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Users/Commonds/DeleteUserCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Transport.Application.UseCases.Users.Commonds
+{
+    public class DeleteUserCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+}
