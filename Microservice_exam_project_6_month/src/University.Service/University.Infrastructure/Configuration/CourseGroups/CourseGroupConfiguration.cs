16f861362712c5fe1149c83ccdbd7a06c6dca20f
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:42:31 +0500

    CourseGroupConfiguration

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Infrastructure/Configuration/CourseGroups/CourseGroupConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using University.Domain.Models;
+
+namespace University.Infrastructure.Configuration.CourseGroups
+{
+    public class CourseGroupConfiguration : IEntityTypeConfiguration<CourseGroup>
+    {
+        public void Configure(EntityTypeBuilder<CourseGroup> builder)
+        {
+            builder.ToTable("CourseGroups");
+
+            builder.HasKey(cg => cg.CourseGroupId);
+
+            builder.Property(cg => cg.CourseId)
+                .IsRequired();
+
+            builder.Property(cg => cg.GroupId)
+                .IsRequired();
+
+           
+
+            builder.HasOne(cg => cg.Group)
+                .WithMany(g => g.Courses)
+                .HasForeignKey(cg => cg.GroupId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
