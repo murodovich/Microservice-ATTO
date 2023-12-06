51be135ceb7e615dfc25c0f3065b52ee24ed0813
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 20:55:46 +0500

    Update

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Routes/Handler/UpdateRouteCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Transport.Application.Absreactions;
+using Transport.Application.UseCases.Routes.Commands;
+using Transport.Domain.Exceptions.Routes;
+
+namespace Transport.Application.UseCases.Routes.Handler
+{
+    public class UpdateRouteCommandHandler : IRequestHandler<UpdateRouteCommand, bool>
+    {
+        private readonly ITransportDBContext _dbContext;
+
+        public UpdateRouteCommandHandler(ITransportDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> Handle(UpdateRouteCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.routes.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (result == null) throw new RouteNotFoundException();
+
+            result.RouteName = request.RouteName;
+            result.StartLocation = request.StartLocation;
+            result.EndLocation = request.EndLocation;
+
+            _dbContext.routes.Update(result);
+
+            var res = await _dbContext.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
+    }
+}
