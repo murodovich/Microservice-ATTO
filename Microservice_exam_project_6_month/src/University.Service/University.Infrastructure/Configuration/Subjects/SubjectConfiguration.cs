b4d8f9551a416ec048170611defeb5d18a2ba3f5
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:49:20 +0500

    University

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Infrastructure/Configuration/Subjects/SubjectConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using University.Domain.Models;
+
+namespace University.Infrastructure.Configuration.Subjects
+{
+    public class SubjectConfiguration : IEntityTypeConfiguration<Subject>
+    {
+        public void Configure(EntityTypeBuilder<Subject> builder)
+        {
+            builder.ToTable("Subjects");
+
+            builder.HasKey(s => s.SubjectId);
+
+            builder.Property(s => s.SubjectName)
+                .IsRequired()
+                .HasMaxLength(255);
+        }
+    }
+}
