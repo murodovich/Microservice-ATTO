e19e9c0da27443558e519e4fcb5efdd292103bd5
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 03:09:08 +0500

    Transport

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Infrastructure/Configuration/Drivers/DriverConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Transport.Domain.Entities.Drivers;
+
+namespace Transport.Infrastructure.Configuration.Drivers
+{
+    public class DriverConfiguration : IEntityTypeConfiguration<Driver>
+    {
+        public void Configure(EntityTypeBuilder<Driver> builder)
+        {
+            builder.ToTable("Drivers");
+
+            builder.HasKey(d => d.Id);
+
+            builder.Property(d => d.DriveName)
+                .IsRequired()
+                .HasMaxLength(255);
+            builder.Property(d => d.LicenseNumber)
+                .IsRequired();
+
+            
+        }
+    }
+}
