9552447b0060e7bd7dcefa13d4c87ee5e6e9bd9d
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 01:37:45 +0500

    GetById

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Transports/Handler/GetByIdTransportQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Transport.Application.Absreactions;
+using Transport.Application.UseCases.Transports.Queries;
+using Transport.Domain.Exceptions.Transports;
+
+namespace Transport.Application.UseCases.Transports.Handler
+{
+    public class GetByIdTransportQueryHandler : IRequestHandler<GetByIdTransportQuery, Domain.Entities.Transports.Transport>
+    {
+        private readonly ITransportDBContext _dbContext;
+
+        public GetByIdTransportQueryHandler(ITransportDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Domain.Entities.Transports.Transport> Handle(GetByIdTransportQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.transports.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (result == null) throw new TransportNotFoundException();
+            return result;
+        }
+    }
+}
