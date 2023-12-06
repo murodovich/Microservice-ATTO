57f22af78dbe9d3e826c3f76d779507a302883a4
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 03:09:21 +0500

    Transport

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Infrastructure/Configuration/Scheldules/SchelduleConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Transport.Domain.Entities.Schedules;
+
+namespace Transport.Infrastructure.Configuration.Scheldules
+{
+    public class SchelduleConfiguration : IEntityTypeConfiguration<Schedule>
+    {
+        public void Configure(EntityTypeBuilder<Schedule> builder)
+        {
+            builder.ToTable("Schedules");
+
+            builder.HasKey(s => s.Id);
+
+            builder.Property(s => s.DepartureTime).IsRequired();
+
+            builder.HasOne(s => s.Route)
+                   .WithMany(r => r.Schedules)
+                   .HasForeignKey(s => s.RouteId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasOne(s => s.Transport)
+                   .WithMany(t => t.schedules)
+                   .HasForeignKey(s => s.TransportId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+        }
+    }
+}
