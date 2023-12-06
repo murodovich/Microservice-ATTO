3d52433f792807beea88d5f405732cd11c1443e5
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:49:25 +0500

    University

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Infrastructure/Configuration/TaskGrads/TaskGradeConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using University.Domain.Models;
+
+namespace University.Infrastructure.Configuration.TaskGrads
+{
+    public class TaskGradeConfiguration : IEntityTypeConfiguration<TaskGrade>
+    {
+        public void Configure(EntityTypeBuilder<TaskGrade> builder)
+        {
+            builder.ToTable("TaskGrades"); 
+
+            builder.HasKey(t => t.TaskGradeId); 
+
+        
+            builder.Property(t => t.FilePath)
+            .IsRequired();
+
+            builder.HasIndex(t => t.DedlineId);
+            builder.HasIndex(t => t.StudentId);
+        }
+    }
+}
