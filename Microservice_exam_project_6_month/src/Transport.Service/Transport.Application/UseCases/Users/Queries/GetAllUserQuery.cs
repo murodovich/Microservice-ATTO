c4a12852d4a725ca1e5a8281523e34ae35695f5a
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:45:43 +0500

    Query

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Users/Queries/GetAllUserQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Transport.Domain.Entities.Users;
+
+namespace Transport.Application.UseCases.Users.Queries
+{
+    public class GetAllUserQuery : IRequest<List<User>>
+    {
+    }
+}
