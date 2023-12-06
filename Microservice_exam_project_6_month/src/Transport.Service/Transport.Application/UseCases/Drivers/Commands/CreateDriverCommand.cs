ac2917223a232335ea450b6688976d7a73f1d254
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 01:06:37 +0500

    Driver

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Drivers/Commands/CreateDriverCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Transport.Application.UseCases.Drivers.Commands
+{
+    public class CreateDriverCommand : IRequest<bool>
+    {
+        public int TransportId { get; set; }
+        public int LicenseNumber { get; set; }
+        public string DriverName { get; set; }
+    }
+}
