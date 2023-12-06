fb382b7e736c1a6ee8e7c3d977b610e383e944dc
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:47:21 +0500

    Transport

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Infrastructure/DependecyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Transport.Application.Absreactions;
+using Transport.Infrastructure.Persistance;
+
+namespace Transport.Infrastructure
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ITransportDBContext, TransportDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefoultConnectionstring"));
+            });
+
+            return services;
+        }
+    }
+}
