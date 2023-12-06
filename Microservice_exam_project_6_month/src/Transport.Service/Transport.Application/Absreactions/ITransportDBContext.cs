c1e299ebe636459420630727967f87b8de7d584a
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:44:14 +0500

    dadsad

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/Absreactions/ITransportDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Transport.Domain.Entities.Drivers;
+using Transport.Domain.Entities.Payments;
+using Transport.Domain.Entities.Routeis;
+using Transport.Domain.Entities.Schedules;
+using Transport.Domain.Entities.Users;
+
+namespace Transport.Application.Absreactions
+{
+    public interface ITransportDBContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Route> routes { get; set; }
+        public DbSet<Driver> drivers { get; set; }
+        public DbSet<Payment> payments { get; set; }
+        public DbSet<Schedule> schedules { get; set; }
+        public DbSet<Domain.Entities.Transports.Transport> transports { get; set; }
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
