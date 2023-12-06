54dd21a484958b2d15329707109b892c0de8556e
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 20:54:42 +0500

    CreateRoute

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Routes/Commands/CreateRouteCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Transport.Application.UseCases.Routes.Commands
+{
+    public class CreateRouteCommand : IRequest<bool>
+    {
+        public string RouteName { get; set; }
+        public string StartLocation { get; set; }
+        public string EndLocation { get; set; }
+    }
+}
