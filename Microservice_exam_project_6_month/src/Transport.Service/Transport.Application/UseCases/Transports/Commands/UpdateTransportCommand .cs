6bd88c0efb97cdd1b7b3af40c04fab846a35df42
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 01:37:10 +0500

    UpdateTransport

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Transports/Commands/UpdateTransportCommand .cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Transport.Application.UseCases.Transports.Commands
+{
+    public class UpdateTransportCommand : IRequest<bool>
+    {
+        public  int Id { get; set; }
+        public string TransportName { get; set; }
+        public string TransportType { get; set; }
+        public int Capacity { get; set; }
+    }
+}
