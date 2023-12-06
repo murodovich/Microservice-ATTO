7c8313d2201dda9bfd17ce515067533bd198731b
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 01:07:03 +0500

    Driver

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Drivers/Handlers/GetAllDriverQueryHandler.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Transport.Application.Absreactions;
+using Transport.Application.UseCases.Drivers.Queries;
+using Transport.Domain.Entities.Drivers;
+using Transport.Domain.Exceptions.Drivers;
+
+namespace Transport.Application.UseCases.Drivers.Handlers
+{
+    public class GetAllDriverQueryHandler : IRequestHandler<GetAllDriverQuery, List<Driver>>
+    {
+        private readonly ITransportDBContext _dbContext;
+
+        public GetAllDriverQueryHandler(ITransportDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Driver>> Handle(GetAllDriverQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.drivers.ToListAsync();
+            if(result == null) throw new DriverNotFoundException();
+
+            return result;
+        }
+    }
+}
