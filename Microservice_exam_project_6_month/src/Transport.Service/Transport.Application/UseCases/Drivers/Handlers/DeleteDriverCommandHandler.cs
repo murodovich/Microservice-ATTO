fad76ea2fc88d8c127e04b3b97f8d4d69c32d2ae
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 01:07:00 +0500

    Driver

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Drivers/Handlers/DeleteDriverCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Transport.Application.Absreactions;
+using Transport.Application.UseCases.Drivers.Commands;
+using Transport.Domain.Exceptions.Drivers;
+
+namespace Transport.Application.UseCases.Drivers.Handlers
+{
+    public class DeleteDriverCommandHandler : IRequestHandler<DeleteDriverCommand, bool>
+    {
+        private readonly ITransportDBContext _dbContext;
+
+        public DeleteDriverCommandHandler(ITransportDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> Handle(DeleteDriverCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.drivers.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (result == null) throw new DriverNotFoundException();
+
+            _dbContext.drivers.Remove(result);
+
+            var res = await _dbContext.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
+    }
+}
