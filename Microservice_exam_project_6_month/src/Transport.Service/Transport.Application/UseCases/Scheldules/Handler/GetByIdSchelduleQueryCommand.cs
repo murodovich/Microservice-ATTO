8c230ab3234d5f2726a2d7a4bac5f24dac13b071
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 02:24:31 +0500

    Scheldule

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Scheldules/Handler/GetByIdSchelduleQueryCommand.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Transport.Application.Absreactions;
+using Transport.Application.UseCases.Scheldules.Queries;
+using Transport.Domain.Entities.Schedules;
+using Transport.Domain.Exceptions.Scheldules;
+
+namespace Transport.Application.UseCases.Scheldules.Handler
+{
+    public class GetByIdSchelduleQueryCommand : IRequestHandler<GetByIdSchelduleQuery, Schedule>
+    {
+        private readonly ITransportDBContext _dbContext;
+
+        public GetByIdSchelduleQueryCommand(ITransportDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Schedule> Handle(GetByIdSchelduleQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.schedules.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (result == null) throw new SchelduleNotFoundException();
+            return result;
+        }
+    }
+}
