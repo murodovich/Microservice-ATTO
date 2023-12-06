8a8e02abe887728dd79ebd5083e1f6e9dbae1b0b
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 20:56:00 +0500

    GetById

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Routes/Queries/GetByIdRouteQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Transport.Domain.Entities.Routeis;
+
+namespace Transport.Application.UseCases.Routes.Queries
+{
+    public class GetByIdRouteQuery : IRequest<Route>
+    {
+        public int Id { get; set; }
+    }
+}
