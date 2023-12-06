74f89971fb8fc243d071ee6122f3b4fcad081809
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:44:05 +0500

    dadsad

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Api/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Transport.Application;
+using Transport.Application.Absreactions;
+using Transport.Infrastructure;
+using Transport.Infrastructure.Persistance;
+namespace Transport.Api
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddInfrastructure(builder.Configuration);
+            builder.Services.AddApplication();
+
+           
+
+
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+
+        }
+    }
+}
