d5ae0e1e80010a2da7b02b80f4dc50fae51e0b68
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 20:55:16 +0500

    DeleterouteHandler

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Routes/Handler/DeleteRouteCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Transport.Application.Absreactions;
+using Transport.Application.UseCases.Routes.Commands;
+using Transport.Domain.Exceptions.Routes;
+
+namespace Transport.Application.UseCases.Routes.Handler
+{
+    public class DeleteRouteCommandHandler : IRequestHandler<DeleteRouteCommand, bool>
+    {
+        private readonly ITransportDBContext _dbContext;
+
+        public DeleteRouteCommandHandler(ITransportDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> Handle(DeleteRouteCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.routes.FirstOrDefaultAsync(x => x.Id == request.Id);
+            if (result == null) throw new RouteNotFoundException();
+
+            _dbContext.routes.Remove(result);
+
+            var res = await _dbContext.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
+    }
+}
