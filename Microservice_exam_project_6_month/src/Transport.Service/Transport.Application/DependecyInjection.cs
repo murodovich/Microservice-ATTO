a1fe800d0f1590e50379a1c440ed9a6d3fa5f2ec
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:46:01 +0500

    Transport

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/DependecyInjection.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Transport.Application
+{
+    public static class DependecyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());  
+            
+            return services;
+        }
+    }
+}
