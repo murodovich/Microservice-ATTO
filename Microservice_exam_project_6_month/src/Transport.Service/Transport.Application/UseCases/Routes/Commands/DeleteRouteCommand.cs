df60e1bd310755ec2cb2a2b4ec9ffabb980d5bcf
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 20:54:49 +0500

    DeleteRoute

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Routes/Commands/DeleteRouteCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Transport.Application.UseCases.Routes.Commands
+{
+    public class DeleteRouteCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+}
