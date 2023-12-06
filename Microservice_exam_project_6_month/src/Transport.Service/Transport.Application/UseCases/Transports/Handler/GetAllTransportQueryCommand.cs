519ecba93eb29764c4a7f9724bd72bb4cb8470f6
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 01:37:33 +0500

    GetAllTransport

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Transports/Handler/GetAllTransportQueryCommand.cs
@@ -0,0 +1,25 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Transport.Application.Absreactions;
+using Transport.Application.UseCases.Transports.Queries;
+using Transport.Domain.Exceptions.Transports;
+
+namespace Transport.Application.UseCases.Transports.Handler
+{
+    public class GetAllTransportQueryCommand : IRequestHandler<GetAllTransportQuery, List<Domain.Entities.Transports.Transport>>
+    {
+        private readonly ITransportDBContext _dbContext;
+
+        public GetAllTransportQueryCommand(ITransportDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Domain.Entities.Transports.Transport>> Handle(GetAllTransportQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.transports.ToListAsync();
+            if (result == null) throw new TransportNotFoundException();
+            return result;
+        }
+    }
+}
