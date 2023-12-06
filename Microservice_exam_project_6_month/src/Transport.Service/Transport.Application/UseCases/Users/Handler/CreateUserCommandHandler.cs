9c34d354df450cd034668d8c3a67bfc87c3a7fca
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:44:54 +0500

    CreateUserHandler

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Users/Handler/CreateUserCommandHandler.cs
@@ -0,0 +1,36 @@
+﻿using MediatR;
+using Transport.Application.Absreactions;
+using Transport.Application.UseCases.Users.Commonds;
+using Transport.Domain.Entities.Users;
+
+namespace Transport.Application.UseCases.Users.Handler
+{
+    public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, bool>
+    {
+        private readonly ITransportDBContext _transportDBContext;
+
+        public CreateUserCommandHandler(ITransportDBContext transportDBContext)
+        {
+            _transportDBContext = transportDBContext;
+        }
+
+        public async Task<bool> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = new User()
+            {
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                UserName = request.UserName,
+                Password = request.Password,
+                PhoneNumber = request.PhoneNumber,
+                CreatedAt = DateTime.UtcNow,
+                Role  = request.Role,
+
+            };
+            await _transportDBContext.Users.AddAsync(user);
+            var result = await _transportDBContext.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
+    }
+}
