5045bf02dbd18d80ad57e1af33ad31ec9c64d195
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:42:48 +0500

    Courseconfiguration

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Infrastructure/Configuration/Courses/CourseConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using University.Domain.Models;
+
+namespace University.Infrastructure.Configuration.Courses
+{
+    public class CourseConfiguration : IEntityTypeConfiguration<Course>
+    {
+        public void Configure(EntityTypeBuilder<Course> builder)
+        {
+            builder.ToTable("Courses"); 
+
+            builder.HasKey(c => c.CourseId); 
+
+            builder.Property(c => c.CourseName)
+                .IsRequired()
+                .HasMaxLength(255); 
+
+            builder.Property(c => c.SubjectId)
+                .IsRequired();
+
+            builder.Property(c => c.TeacherId)
+                .IsRequired();
+
+            builder.HasMany(c => c.Courses)
+                .WithOne(cg => cg.Course)
+                .HasForeignKey(cg => cg.CourseId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
