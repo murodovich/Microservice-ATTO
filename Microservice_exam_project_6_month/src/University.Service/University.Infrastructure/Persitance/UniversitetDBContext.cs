d3f4a67d47122ef5a0b310639887da5b2ac2e4bb
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:49:32 +0500

    University

--- Microservice_exam_project_6_month/src/University.Service/University.Infrastructure/Persitance/UniversitetDBContext.cs
+++ Microservice_exam_project_6_month/src/University.Service/University.Infrastructure/Persitance/UniversitetDBContext.cs
@@ -1,6 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using University.Application.Absreactions;
 using University.Domain.Models;
+using University.Infrastructure.Configuration.Attendanceis;
+using University.Infrastructure.Configuration.CourseGroups;
+using University.Infrastructure.Configuration.Courses;
+using University.Infrastructure.Configuration.Dedlines;
+using University.Infrastructure.Configuration.Groups;
+using University.Infrastructure.Configuration.Lessons;
+using University.Infrastructure.Configuration.Student;
+using University.Infrastructure.Configuration.Subjects;
+using University.Infrastructure.Configuration.TaskGrads;
+using University.Infrastructure.Configuration.Teachers;
 
 namespace University.Infrastructure.Persitance
 {
@@ -18,6 +28,23 @@ namespace University.Infrastructure.Persitance
         public DbSet<Dedline> dededlines { get; set; }
         public DbSet<CourseGroup> coursegroups { get; set; }
         public DbSet<Attendance> attendances { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new AttendanceConfiguration());
+            modelBuilder.ApplyConfiguration(new CourseGroupConfiguration());
+            modelBuilder.ApplyConfiguration(new CourseConfiguration());
+            modelBuilder.ApplyConfiguration(new DedlineConfiguration());
+            modelBuilder.ApplyConfiguration(new GroupConfiguration());
+            modelBuilder.ApplyConfiguration(new LessonConfiguration());
+            modelBuilder.ApplyConfiguration(new StudentConfiguration());
+            modelBuilder.ApplyConfiguration(new SubjectConfiguration());
+            modelBuilder.ApplyConfiguration(new TaskGradeConfiguration());
+            modelBuilder.ApplyConfiguration(new TeacherConfiguration());
+
+        }
 
     }
 }
