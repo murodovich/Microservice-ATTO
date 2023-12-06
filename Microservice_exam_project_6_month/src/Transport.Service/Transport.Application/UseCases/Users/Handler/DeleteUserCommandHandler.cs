9b6a9c92cdc2d7932d7afdc090a38b630c564cf7
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:45:01 +0500

    DeleteHandler

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Users/Handler/DeleteUserCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Transport.Application.Absreactions;
+using Transport.Application.UseCases.Users.Commonds;
+
+namespace Transport.Application.UseCases.Users.Handler
+{
+    public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, bool>
+    {
+        private readonly ITransportDBContext _dbContext;
+
+        public DeleteUserCommandHandler(ITransportDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id == request.Id);
+
+            if (result == null) { return false; }
+
+            _dbContext.Users.Remove(result);
+            var res = await _dbContext.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
+    }
+}
