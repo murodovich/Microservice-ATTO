028cdbe28c9d4803c7a6d41c215efecbf12279b6
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 03:09:13 +0500

    Transport

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Infrastructure/Configuration/Payments/PaymentConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Transport.Domain.Entities.Payments;
+
+namespace Transport.Infrastructure.Configuration.Payments
+{
+    public class PaymentConfiguration : IEntityTypeConfiguration<Payment>
+    {
+        public void Configure(EntityTypeBuilder<Payment> builder)
+        {
+            builder.ToTable("Payments");
+
+            builder.HasKey(p => p.Id);
+
+            builder.Property(p => p.Amaunt)
+                .IsRequired();
+            builder.Property(p => p.PaymentDate)
+                .IsRequired();
+
+            builder.HasOne(p => p.User)
+                   .WithMany(u => u.Payment)
+                   .HasForeignKey(p => p.UserId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+           
+
+        }
+    }
+}
