9a7f99be1b474d7db7bd0c80391391d5e08a5f41
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 01:07:20 +0500

    Driver

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Drivers/Queries/GetByIdDriverQuery.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using Transport.Domain.Entities.Drivers;
+
+namespace Transport.Application.UseCases.Drivers.Queries
+{
+    public class GetByIdDriverQuery : IRequest<Driver>
+    {
+        public int Id { get; set; }
+
+    }
+}
