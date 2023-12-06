b896068f7d2133a9bfacc3bc56175673154158a6
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 20:55:22 +0500

    GetALlRoute

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Routes/Handler/GetAllRouteQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Transport.Application.Absreactions;
+using Transport.Application.UseCases.Routes.Queries;
+using Transport.Domain.Entities.Routeis;
+using Transport.Domain.Exceptions.Routes;
+
+namespace Transport.Application.UseCases.Routes.Handler
+{
+    public class GetAllRouteQueryHandler : IRequestHandler<GetAllRouteQuery, List<Route>>
+    {
+        private readonly ITransportDBContext _dbContext;
+
+        public GetAllRouteQueryHandler(ITransportDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Route>> Handle(GetAllRouteQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.routes.ToListAsync();
+            if (result == null) throw new RouteNotFoundException();
+            return result;
+        }
+    }
+}
