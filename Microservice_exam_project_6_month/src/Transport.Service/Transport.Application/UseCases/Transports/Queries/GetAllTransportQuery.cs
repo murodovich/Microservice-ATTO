90a8b97b343c76fe5e86a9a1cc3edfb11ea7b89f
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 01:38:04 +0500

    Getall

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Transports/Queries/GetAllTransportQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace Transport.Application.UseCases.Transports.Queries
+{
+    public class GetAllTransportQuery : IRequest<List<Domain.Entities.Transports.Transport>>
+    { 
+    }
+}
