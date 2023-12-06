64b2369be1f2e119990a994d09ec84a3350f6f5c
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 02:24:42 +0500

    Scheldule

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Scheldules/Queries/GetByIdSchelduleQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Transport.Domain.Entities.Schedules;
+
+namespace Transport.Application.UseCases.Scheldules.Queries
+{
+    public class GetByIdSchelduleQuery : IRequest<Schedule>
+    {
+        public int Id { get; set; }
+    }
+}
