90c81d9a84787d353448e0800f93fbe905407213
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 01:06:56 +0500

    Driver

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Drivers/Handlers/CreateDriverCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Transport.Application.Absreactions;
+using Transport.Application.UseCases.Drivers.Commands;
+using Transport.Domain.Entities.Drivers;
+
+namespace Transport.Application.UseCases.Drivers.Handlers
+{
+    public class CreateDriverCommandHandler : IRequestHandler<CreateDriverCommand,bool>
+    {
+        private readonly ITransportDBContext _dbContext;
+
+        public CreateDriverCommandHandler(ITransportDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> Handle(CreateDriverCommand request, CancellationToken cancellationToken)
+        {
+            var driver = new Driver()
+            {
+                DriveName = request.DriverName,
+                LicenseNumber = request.LicenseNumber,
+                TransportId = request.TransportId,
+            };
+
+            await _dbContext.drivers.AddAsync(driver);
+
+            var result = await _dbContext.SaveChangesAsync(cancellationToken);
+            return result > 0;
+        }
+    }
+}
