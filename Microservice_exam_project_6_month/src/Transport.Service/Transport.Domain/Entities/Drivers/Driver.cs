821e6f148ac9bae4589d83a13dae81caeca550a3
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:46:20 +0500

    Transport

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Domain/Entities/Drivers/Driver.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Transport.Domain.Entities.Drivers
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string DriveName { get; set; }
+        public int LicenseNumber { get; set; }
+        [ForeignKey(nameof(Domain.Entities.Transports.Transport))]
+        public int TransportId { get; set; }
+        public Domain.Entities.Transports.Transport Transport { get; set; }
+
+    }
+}
