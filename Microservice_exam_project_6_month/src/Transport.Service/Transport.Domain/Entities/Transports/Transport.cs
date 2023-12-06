e1fb7204677a513cffc9a5c26e576d7dcce1254c
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:46:38 +0500

    Transport

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Domain/Entities/Transports/Transport.cs
@@ -0,0 +1,19 @@
+﻿using Transport.Domain.Entities.Drivers;
+using Transport.Domain.Entities.Payments;
+using Transport.Domain.Entities.Schedules;
+
+namespace Transport.Domain.Entities.Transports
+{
+    public class Transport
+    {
+        public int Id { get; set; }
+        public string TransportName { get; set; }
+        public string TransportType { get; set; }
+        public int Capacity { get; set; }
+        public ICollection<Payment> payments { get; set; }
+        public ICollection<Driver> drivers { get; set; }
+        public ICollection<Schedule> schedules { get; set; }
+
+        
+    }
+}
