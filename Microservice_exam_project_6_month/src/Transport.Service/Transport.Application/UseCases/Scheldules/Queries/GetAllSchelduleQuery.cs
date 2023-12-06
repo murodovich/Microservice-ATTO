2993ef135bcfa0002f4a9707f891dfbfc1be0c4b
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 02:24:39 +0500

    Scheldule

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Scheldules/Queries/GetAllSchelduleQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Transport.Domain.Entities.Schedules;
+
+namespace Transport.Application.UseCases.Scheldules.Queries
+{
+    public class GetAllSchelduleQuery : IRequest<List<Schedule>>
+    {
+    }
+}
