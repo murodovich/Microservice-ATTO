169b9cfbeee994fccff66513f891666f8f5a00e7
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:44:45 +0500

    UpdateUserCommand

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Users/Commonds/UpdateUserCommand.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using Transport.Domain.Enums;
+
+namespace Transport.Application.UseCases.Users.Commonds
+{
+    public class UpdateUserCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
