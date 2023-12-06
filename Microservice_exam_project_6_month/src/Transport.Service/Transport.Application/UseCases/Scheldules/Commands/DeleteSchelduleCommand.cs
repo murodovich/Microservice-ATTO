6e57bc204c02dcd1839afb40b8e76a9f65db77b5
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 02:24:10 +0500

    Scheldule

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Scheldules/Commands/DeleteSchelduleCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Transport.Application.UseCases.Scheldules.Commands
+{
+    public class DeleteSchelduleCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+}
