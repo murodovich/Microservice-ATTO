b14f61d4e92982ef2d19b89f7cf2f2eeaca53058
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:45:11 +0500

    GetallHandler

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Users/Handler/GetAllUserQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Transport.Application.Absreactions;
+using Transport.Application.UseCases.Users.Queries;
+using Transport.Domain.Entities.Users;
+
+namespace Transport.Application.UseCases.Users.Handler
+{
+    public class GetAllUserQueryHandler : IRequestHandler<GetAllUserQuery, List<User>>
+    {
+        private readonly ITransportDBContext _dbContext;
+
+        public GetAllUserQueryHandler(ITransportDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<User>> Handle(GetAllUserQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.Users.ToListAsync();
+            return result;
+        }
+    }
+}
