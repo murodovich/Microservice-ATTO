662f2ed5dd922b6a5fd0d0463c6f19627cebda7e
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:46:44 +0500

    Transport

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Domain/Entities/Users/User.cs
@@ -0,0 +1,21 @@
+﻿using Transport.Domain.Entities.Payments;
+using Transport.Domain.Enums;
+
+namespace Transport.Domain.Entities.Users
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Role Role { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public ICollection<Payment> Payment { get; set; }
+
+    }
+}
