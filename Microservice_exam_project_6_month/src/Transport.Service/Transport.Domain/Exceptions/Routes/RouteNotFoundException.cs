0d8d3c0c52289d200a6281a12e7ee62485d36328
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 20:11:56 +0500

    Route

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Domain/Exceptions/Routes/RouteNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Transport.Domain.Exceptions.Routes
+{
+    public class RouteNotFoundException : NotFoundException
+    {
+        public RouteNotFoundException()
+        {
+            this.TitleMessage = "Route not Found!";
+        }
+    }
+}
