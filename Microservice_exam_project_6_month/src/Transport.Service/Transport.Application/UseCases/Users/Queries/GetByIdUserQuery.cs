7e781f6a3da968e7e4998645990cdaadeec3c4fd
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:45:57 +0500

    Transport

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Users/Queries/GetByIdUserQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Transport.Domain.Entities.Users;
+
+namespace Transport.Application.UseCases.Users.Queries
+{
+    public class GetByIdUserQuery : IRequest<User>
+    {
+        public int Id { get; set; }
+    }
+}
