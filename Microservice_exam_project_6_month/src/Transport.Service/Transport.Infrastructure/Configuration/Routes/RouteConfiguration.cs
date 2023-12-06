03838d4451caa361cd11ed7f9dc6521fc4a2bed3
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 03:09:17 +0500

    Transport

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Infrastructure/Configuration/Routes/RouteConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Transport.Domain.Entities.Routeis;
+
+namespace Transport.Infrastructure.Configuration.Routes
+{
+    public class RouteConfiguration : IEntityTypeConfiguration<Route>
+    {
+        public void Configure(EntityTypeBuilder<Route> builder)
+        {
+            builder.ToTable("Routes");
+
+            builder.HasKey(r => r.Id);
+
+            builder.Property(r => r.RouteName).IsRequired().HasMaxLength(255);
+            builder.Property(r => r.StartLocation).IsRequired().HasMaxLength(255);
+            builder.Property(r => r.EndLocation).IsRequired().HasMaxLength(255);
+
+            builder.HasMany(r => r.Schedules)
+                   .WithOne(s => s.Route)
+                   .HasForeignKey(s => s.RouteId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+        }
+    }
+}
