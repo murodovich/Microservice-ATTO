1e67e3fbb9b4f3a881f04159f30321cda2037b21
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 02:24:14 +0500

    Scheldule

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Scheldules/Commands/UpdateSchelduleCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Transport.Application.UseCases.Scheldules.Commands
+{
+    public class UpdateSchelduleCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+        public DateTime DepartureTime { get; set; }
+        public int TransportId { get; set; }
+        public int RouteId { get; set; }
+    }
+}
