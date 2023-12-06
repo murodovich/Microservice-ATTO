2aa81c074e1246b27060e227f3d353fe6cc98ec3
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 02:24:18 +0500

    Scheldule

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Scheldules/Handler/CreateSchelduleCommandHandler.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Transport.Application.Absreactions;
+using Transport.Application.UseCases.Scheldules.Commands;
+using Transport.Domain.Entities.Schedules;
+
+namespace Transport.Application.UseCases.Scheldules.Handler
+{
+    public class CreateSchelduleCommandHandler : IRequestHandler<CreateSchelduleCommand, bool>
+    {
+        private readonly ITransportDBContext _dbContext;
+
+        public CreateSchelduleCommandHandler(ITransportDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> Handle(CreateSchelduleCommand request, CancellationToken cancellationToken)
+        {
+            var result = new Schedule()
+            {
+                DepartureTime = DateTime.UtcNow,
+                TransportId = request.TransportId,
+                RouteId = request.RouteId,
+            };
+
+            await _dbContext.schedules.AddAsync(result);
+
+            var res = await _dbContext.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
+    }
+}
