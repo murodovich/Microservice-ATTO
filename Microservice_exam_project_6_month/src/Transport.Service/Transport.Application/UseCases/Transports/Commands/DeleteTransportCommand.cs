96f33326154d5b341e238be2b035924b895f8167
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 01:36:58 +0500

    DeletedTransport

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Transports/Commands/DeleteTransportCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace Transport.Application.UseCases.Transports.Commands
+{
+    public class DeleteTransportCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+    }
+}
