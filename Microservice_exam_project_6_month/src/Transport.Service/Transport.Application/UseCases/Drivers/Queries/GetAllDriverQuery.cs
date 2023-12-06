00dbc8cb44c75433b62ad1c4d46e243c82cf80aa
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 01:07:15 +0500

    Driver

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Drivers/Queries/GetAllDriverQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Transport.Domain.Entities.Drivers;
+
+namespace Transport.Application.UseCases.Drivers.Queries
+{
+    public class GetAllDriverQuery : IRequest<List<Driver>>
+    {
+    }
+}
