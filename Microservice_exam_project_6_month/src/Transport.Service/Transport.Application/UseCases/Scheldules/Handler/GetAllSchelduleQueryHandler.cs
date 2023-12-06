ba660d85d4c958bfb0b834889494f9c7cb011015
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 02:24:27 +0500

    Scheldule

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Scheldules/Handler/GetAllSchelduleQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Transport.Application.Absreactions;
+using Transport.Application.UseCases.Scheldules.Queries;
+using Transport.Domain.Entities.Schedules;
+
+namespace Transport.Application.UseCases.Scheldules.Handler
+{
+    public class GetAllSchelduleQueryHandler : IRequestHandler<GetAllSchelduleQuery, List<Schedule>>
+    {
+        private readonly ITransportDBContext _dbContext;
+
+        public GetAllSchelduleQueryHandler(ITransportDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Schedule>> Handle(GetAllSchelduleQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.schedules.ToListAsync();
+            return result;
+        }
+    }
+}
