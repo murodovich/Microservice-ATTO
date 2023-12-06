ac60880275e7dbabdfe1534388b086a6f938658a
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:47:59 +0500

    University

--- Microservice_exam_project_6_month/src/University.Service/University.Api/Program.cs
+++ Microservice_exam_project_6_month/src/University.Service/University.Api/Program.cs
@@ -1,12 +1,12 @@
 using University.Application;
 using University.Infrastructure;
 
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddAplication();
 
@@ -15,7 +15,6 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
