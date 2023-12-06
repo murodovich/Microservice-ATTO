c746fdf3c7007c7092b6c8f32ff20d433d2b1b57
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:43:16 +0500

    Teacher

--- Microservice_exam_project_6_month/src/University.Service/University.Domain/Models/Teacher.cs
+++ Microservice_exam_project_6_month/src/University.Service/University.Domain/Models/Teacher.cs
@@ -8,6 +8,9 @@ namespace University.Domain.Models
         public string LastName { get; set; }
         public string FirstName { get; set; }
         public string FatherName { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
         public string Direction { get; set; }
         public Gender Gender { get; set; }
         public Role Role { get; set; }
