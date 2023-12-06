2fe0f7d167805fb2d26d0d81062df57dd9e2d925
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 20:11:49 +0500

    Payment

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Domain/Exceptions/Payments/PaymentNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Transport.Domain.Exceptions.Payments
+{
+    public class PaymentNotFoundException : NotFoundException
+    {
+        public PaymentNotFoundException()
+        {
+            this.TitleMessage = "Payment Not Found!";
+        }
+    }
+}
