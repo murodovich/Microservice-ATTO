08d387b96aaa8ffdbe58a353a11be32357cdc257
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:48:47 +0500

    University

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Infrastructure/Configuration/Dedlines/DedlineConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using University.Domain.Models;
+
+namespace University.Infrastructure.Configuration.Dedlines
+{
+    public class DedlineConfiguration : IEntityTypeConfiguration<Dedline>
+    {
+        public void Configure(EntityTypeBuilder<Dedline> builder)
+        {
+            builder.ToTable("Dedlines"); 
+
+            builder.HasKey(d => d.DedlineId); 
+
+            builder.Property(d => d.ExpiredDate)
+                .IsRequired();
+
+            builder.Property(d => d.MaxGrade)
+                .IsRequired();
+
+            builder.Property(d => d.StartDate)
+                .IsRequired()
+                .HasDefaultValueSql("GETDATE()"); 
+
+            builder.Property(d => d.FilePage)
+                .IsRequired();
+
+        }
+    }
+}
