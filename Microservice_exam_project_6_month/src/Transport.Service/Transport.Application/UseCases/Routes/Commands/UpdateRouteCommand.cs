4d81ed6a65ddd98654ec1737c63d7a904ebbe1fe
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 20:54:57 +0500

    UpdateRoute

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Routes/Commands/UpdateRouteCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace Transport.Application.UseCases.Routes.Commands
+{
+    public class UpdateRouteCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+        public string RouteName { get; set; }
+        public string StartLocation { get; set; }
+        public string EndLocation { get; set; }
+    }
+}
