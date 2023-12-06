9960a5021126dff9b6cd6412478f373f2b3ee4b6
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:45:24 +0500

    GetByIdHanler

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Users/Handler/GetByIdUserQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Transport.Application.Absreactions;
+using Transport.Application.UseCases.Users.Queries;
+using Transport.Domain.Entities.Users;
+
+namespace Transport.Application.UseCases.Users.Handler
+{
+    public class GetByIdUserQueryHandler : IRequestHandler<GetByIdUserQuery, User>
+    {
+        private readonly ITransportDBContext _dbContext;
+
+        public GetByIdUserQueryHandler(ITransportDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<User> Handle(GetByIdUserQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.Users.FirstOrDefaultAsync(x => x.Id == request.Id);
+            return result;
+        }
+    }
+}
