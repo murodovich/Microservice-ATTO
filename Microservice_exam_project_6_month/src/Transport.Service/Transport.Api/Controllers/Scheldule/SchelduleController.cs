b59d721dc6a3e6085f13d6a8fda389d053dbf33b
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 02:23:49 +0500

    Scheldule

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Api/Controllers/Scheldule/SchelduleController.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Transport.Application.UseCases.Scheldules.Commands;
+using Transport.Application.UseCases.Scheldules.Queries;
+
+namespace Transport.Api.Controllers.Scheldule
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class SchelduleController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public SchelduleController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllScheldule()
+        {
+            var result = await _mediator.Send(new GetAllSchelduleQuery());
+            return Ok(result);
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateScheldule(CreateSchelduleCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Created");
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetByIdScheldule(int Id) 
+        {
+            var result = await _mediator.Send(new GetByIdSchelduleQuery() { Id = Id });
+            return Ok(result);
+        }
+        [HttpPut]
+        public async ValueTask<IActionResult> UpdateScheldule(UpdateSchelduleCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Updated");
+        }
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteScheldule(int Id)
+        {
+            await _mediator.Send(new DeleteSchelduleCommand() { Id = Id });
+            return Ok("Deleted");
+        }
+    }
+}
