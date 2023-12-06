474d171f9522e6f8d80987148e81f2dd4980e2e7
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-06 19:48:07 +0500

    University

---
+++ Microservice_exam_project_6_month/src/University.Service/University.Application/PaswordHash/Hash512.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Application.PaswordHash
+{
+    public class Hash512
+    {
+        public static string ComputeSHA512HashFromString(string input)
+        {
+            using (SHA512 sha512 = SHA512.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+
+                byte[] hashBytes = sha512.ComputeHash(inputBytes);
+
+                return BitConverter.ToString(hashBytes).Replace("-", "");
+            }
+        }
+    }
+}
