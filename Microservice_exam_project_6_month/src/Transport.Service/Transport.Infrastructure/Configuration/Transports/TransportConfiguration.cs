03d3f0da7da8700bd4d124ec3c09d8f9f2dc06c4
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 03:09:26 +0500

    Transport

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Infrastructure/Configuration/Transports/TransportConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Transport.Domain.Entities.Transports;
+
+namespace Transport.Infrastructure.Configuration.Transports
+{
+    public class TransportConfiguration : IEntityTypeConfiguration<Domain.Entities.Transports.Transport>
+    {
+        public void Configure(EntityTypeBuilder<Domain.Entities.Transports.Transport> builder)
+        {
+            builder.ToTable("Transports");
+
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.TransportName).IsRequired().HasMaxLength(255);
+            builder.Property(t => t.TransportType).IsRequired().HasMaxLength(50);
+            builder.Property(t => t.Capacity).IsRequired();
+
+            
+
+            builder.HasMany(t => t.drivers)
+                   .WithOne(d => d.Transport)
+                   .HasForeignKey(d => d.TransportId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasMany(t => t.schedules)
+                   .WithOne(s => s.Transport)
+                   .HasForeignKey(s => s.TransportId)
+                   .OnDelete(DeleteBehavior.Restrict);
+
+        }
+    }
+}
