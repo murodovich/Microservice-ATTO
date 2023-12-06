4b27ac1a53d02703c7ddf583e3b86919b769f3a5
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 01:07:40 +0500

    Transport

---
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Application/UseCases/Transports/Handler/CreateTransportCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using MediatR;
+using Transport.Application.Absreactions;
+using Transport.Application.UseCases.Transports.Commands;
+
+namespace Transport.Application.UseCases.Transports.Handler
+{
+    public class CreateTransportCommandHandler : IRequestHandler<CreateTransportCommand, bool>
+    {
+        private readonly ITransportDBContext _dbContext;
+
+        public CreateTransportCommandHandler(ITransportDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public Task<bool> Handle(CreateTransportCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
