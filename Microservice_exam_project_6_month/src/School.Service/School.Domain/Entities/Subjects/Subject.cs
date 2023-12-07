d3dacfbdf57116bd466ae20ee4dd0e62469d252b
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 10:34:42 +0500

    Subject

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Domain/Entities/Subjects/Subject.cs
@@ -0,0 +1,11 @@
+﻿using School.Domain.Entities.Courses;
+
+namespace School.Domain.Entities.Subjects
+{
+    public class Subject
+    {
+        public int SubjectId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Course> Courses { get; set; }
+    }
+}
