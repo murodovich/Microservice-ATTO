4a5f2d55694980bf77ff800b640b56c5cd7550c2
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 14:28:22 +0500

    Student

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Infrastructure/Migrations/20231207083608_SchoolDb2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace School.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SchoolDb2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "teachers",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "teachers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "students",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "students",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "teachers");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "teachers");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "students");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedAt",
+                table: "students");
+        }
+    }
+}
