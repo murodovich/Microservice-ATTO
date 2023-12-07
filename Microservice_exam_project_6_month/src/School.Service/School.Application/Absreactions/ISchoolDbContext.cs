eebd2123667a94d40e1674f683e3981aa5f5f400
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 11:08:42 +0500

    School

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/Absreactions/ISchoolDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using School.Domain.Entities.Courses;
+using School.Domain.Entities.StudentCourses;
+using School.Domain.Entities.Students;
+using School.Domain.Entities.Subjects;
+using School.Domain.Entities.Teachers;
+
+namespace School.Application.Absreactions
+{
+    public interface ISchoolDbContext
+    {
+        public DbSet<Student> students { get; set; }
+        public DbSet<Teacher> teachers { get; set; }
+        public DbSet<Subject> subjects { get; set; }
+        public DbSet<Course> courses { get; set; }
+        public DbSet<StudentCourse> studentsCourse { get; set; }
+        public Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+
+    }
+}
