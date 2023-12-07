2774a14c88b2dcbe652716d29eb5ecff69ff7fda
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 10:34:22 +0500

    COurse

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Domain/Entities/Courses/Course.cs
@@ -0,0 +1,20 @@
+﻿using School.Domain.Entities.StudentCourses;
+using School.Domain.Entities.Subjects;
+using School.Domain.Entities.Teachers;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace School.Domain.Entities.Courses
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string Name { get; set; }
+        [ForeignKey(nameof(Subject))]
+        public int SubjectId { get; set; }
+        public Subject Subject { get; set; }
+        [ForeignKey(nameof(Teachers))]
+        public int TeacherId { get; set; }
+        public Teacher Teacher { get; set; }
+        public ICollection<StudentCourse> StudentCourses { get; set; }
+    }
+}
