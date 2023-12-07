175a8e9e5f3894e8d8b0633493540b6128b90a5f
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 14:27:49 +0500

    Student

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/UseCases/Students/Handler/DeleteStudentCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using School.Application.Absreactions;
+using School.Application.UseCases.Students.Commands;
+using School.Domain.Exceptions.Students;
+
+namespace School.Application.UseCases.Students.Handler
+{
+    public class DeleteStudentCommandHandler : IRequestHandler<DeleteStudentCommand, bool>
+    {
+        private readonly ISchoolDbContext _dbContext;
+
+        public DeleteStudentCommandHandler(ISchoolDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> Handle(DeleteStudentCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.students.FirstOrDefaultAsync(x => x.StudentId == request.Id);
+            if (result == null) throw new StudentNotFoundException();
+            
+            _dbContext.students.Remove(result);
+            var res = await _dbContext.SaveChangesAsync(cancellationToken);
+            return res > 0;
+
+        }
+    }
+}
