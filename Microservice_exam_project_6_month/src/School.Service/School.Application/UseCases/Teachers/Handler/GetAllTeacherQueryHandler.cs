24937900356e0536a749f93c2ad102451ab1525d
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 15:40:35 +0500

    GetAll

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/UseCases/Teachers/Handler/GetAllTeacherQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using School.Application.Absreactions;
+using School.Application.UseCases.Teachers.Queries;
+using School.Domain.Entities.Teachers;
+
+namespace School.Application.UseCases.Teachers.Handler
+{
+    public class GetAllTeacherQueryHandler : IRequestHandler<GetAllTeacherQuery, List<Teacher>>
+    {
+        private readonly ISchoolDbContext _context;
+
+        public GetAllTeacherQueryHandler(ISchoolDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Teacher>> Handle(GetAllTeacherQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _context.teachers.ToListAsync();
+            return result;
+        }
+    }
+}
