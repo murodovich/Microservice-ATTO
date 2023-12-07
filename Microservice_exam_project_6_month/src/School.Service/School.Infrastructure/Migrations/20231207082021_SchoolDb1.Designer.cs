59a06822d03d29b608bd55fb8220efb14ce05259
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 13:31:48 +0500

    migra

--- Microservice_exam_project_6_month/src/School.Service/School.Infrastructure/Migrations/20231207082021_SchoolDb1.Designer.cs
+++ Microservice_exam_project_6_month/src/School.Service/School.Infrastructure/Migrations/20231207082021_SchoolDb1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using School.Infrastructure.Persistance;
 
@@ -11,9 +12,11 @@ using School.Infrastructure.Persistance;
 namespace School.Infrastructure.Migrations
 {
     [DbContext(typeof(SchoolDBContext))]
-    partial class SchoolDBContextModelSnapshot : ModelSnapshot
+    [Migration("20231207082021_SchoolDb1")]
+    partial class SchoolDb1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
