4356ed2faf69456a7ffea23fa37de3fd13d88294
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 15:40:58 +0500

    dada

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Infrastructure/Migrations/20231207093945_Stud.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace School.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Stud : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Gender",
+                table: "teachers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Gender",
+                table: "students",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "teachers");
+
+            migrationBuilder.DropColumn(
+                name: "Gender",
+                table: "students");
+        }
+    }
+}
