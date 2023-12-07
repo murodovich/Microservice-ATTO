36d7d7a3d43cadb85f55b4397fa2fca373b519fc
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 15:39:40 +0500

    save

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Api/Controllers/Teachers/TeacherController.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using School.Application.UseCases.Teachers.Commands;
+using School.Application.UseCases.Teachers.Queries;
+
+namespace School.Api.Controllers.Teachers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeacherController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public TeacherController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllTeacherSchool()
+        {
+            var result = await _mediator.Send(new GetAllTeacherQuery());
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateTeacherSchool(CreateTeacherCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Created");
+        }
+    }
+}
