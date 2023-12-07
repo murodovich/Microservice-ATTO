e40a9d38956049952bbc50048aaf5b18ded31678
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 14:28:03 +0500

    Student

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/UseCases/Students/Queries/GetAllStudentQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using School.Domain.Entities.Students;
+
+namespace School.Application.UseCases.Students.Queries
+{
+    public class GetAllStudentQuery : IRequest<List<Student>>
+    {
+    }
+}
