5081c37845a494f74475575bda8a341aee853fc7
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 13:31:15 +0500

    Student

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Infrastructure/Configuration/StudentCourses/StudentCourseConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using School.Domain.Entities.StudentCourses;
+
+namespace School.Infrastructure.Configuration.StudentCourses
+{
+    public class StudentCourseConfiguration : IEntityTypeConfiguration<StudentCourse>
+    {
+        public void Configure(EntityTypeBuilder<StudentCourse> builder)
+        {
+            builder.HasKey(sc => sc.StudentCourseId);
+
+            builder.Property(sc => sc.Grade)
+                .IsRequired(false);
+
+            builder.HasOne(sc => sc.Student)
+                .WithMany(s => s.StudentCourses)
+                .HasForeignKey(sc => sc.StudentId);
+
+            builder.HasOne(sc => sc.Course)
+                .WithMany(c => c.StudentCourses)
+                .HasForeignKey(sc => sc.CourseId);
+
+        }
+    }
+}
