c1c19c6b4b974e4b8fb530e18cfb70b992316ff6
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 13:30:18 +0500

    Courses

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Domain/Exceptions/Courses/CourseNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace School.Domain.Exceptions.Courses
+{
+    public class CourseNotFoundException : NotFoundException
+    {
+        public CourseNotFoundException()
+        {
+            this.TitleMessage = "Course Not Found!";
+        }
+    }
+}
