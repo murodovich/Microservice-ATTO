86ebec3088f1ba81dd8f853a4fb57260d451401e
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 14:27:34 +0500

    Student

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/UseCases/Students/Commands/CreateStudentCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace School.Application.UseCases.Students.Commands
+{
+    public class CreateStudentCommand : IRequest<bool>
+    {
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public DateTime BirthDate { get; set; }
+        
+    }
+}
