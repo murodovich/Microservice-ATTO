3150f400f3e819d95b8592ff6bb92a13a0fff1c8
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 11:09:16 +0500

    school

--- Microservice_exam_project_6_month/src/School.Service/School.Infrastructure/Persistance/SchoolDBContext.cs
+++ Microservice_exam_project_6_month/src/School.Service/School.Infrastructure/Persistance/SchoolDBContext.cs
@@ -1,15 +1,29 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using School.Application.Absreactions;
+using School.Domain.Entities.Courses;
+using School.Domain.Entities.StudentCourses;
+using School.Domain.Entities.Students;
+using School.Domain.Entities.Subjects;
+using School.Domain.Entities.Teachers;
 
 namespace School.Infrastructure.Persistance
 {
-    public class SchoolDBContext : DbContext
+    public class SchoolDBContext : DbContext, ISchoolDbContext
     {
-        public SchoolDBContext(DbContextOptions<SchoolDBContext> options) 
+        public SchoolDBContext(DbContextOptions<SchoolDBContext> options)
             : base(options)
         {
-            
+
         }
 
-        
+        public DbSet<Student> students { get; set; }
+        public DbSet<Teacher> teachers { get; set; }
+        public DbSet<Subject> subjects { get; set; }
+        public DbSet<Course> courses { get; set; }
+        public DbSet<StudentCourse> studentsCourse { get; set; }
+
+
+
+
     }
 }
