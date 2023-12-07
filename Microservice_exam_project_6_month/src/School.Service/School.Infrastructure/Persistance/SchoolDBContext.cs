5d4c549ae6ddb28ebd36f9b8ff6cd3ac7a8bb1ea
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 10:35:21 +0500

    School

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Infrastructure/Persistance/SchoolDBContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace School.Infrastructure.Persistance
+{
+    public class SchoolDBContext : DbContext
+    {
+        public SchoolDBContext(DbContextOptions<SchoolDBContext> options) 
+            : base(options)
+        {
+            
+        }
+
+        
+    }
+}
