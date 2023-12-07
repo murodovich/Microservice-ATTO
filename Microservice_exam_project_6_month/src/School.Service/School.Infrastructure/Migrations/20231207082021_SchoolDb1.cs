6f356ef89e7489fcbe6fe9b82eeb69f500f0cb4b
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 13:31:42 +0500

    Migration

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Infrastructure/Migrations/20231207082021_SchoolDb1.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace School.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SchoolDb1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "teachers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "teachers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserName",
+                table: "teachers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "teachers");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "teachers");
+
+            migrationBuilder.DropColumn(
+                name: "UserName",
+                table: "teachers");
+        }
+    }
+}
