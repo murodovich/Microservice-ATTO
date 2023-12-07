6e24dfbf659799475e8239b1f234fca0c6e55df3
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 13:30:46 +0500

    Teacher

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Domain/Exceptions/Teachers/TeacherNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace School.Domain.Exceptions.Teachers
+{
+    public class TeacherNotFoundException : NotFoundException
+    {
+        public TeacherNotFoundException()
+        {
+            this.TitleMessage = "Teacher not Found!";
+        }
+    }
+}
