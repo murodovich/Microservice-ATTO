56fef976857f29caad6efd9bfb27f8f06de59a30
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 11:09:19 +0500

    school

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Infrastructure/DependecyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using School.Application.Absreactions;
+using School.Infrastructure.Persistance;
+
+namespace School.Infrastructure
+{
+    public static class DependecyInjection 
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ISchoolDbContext,SchoolDBContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefoultConnectionstring"));
+            });
+
+            return services;
+        }
+    }
+}
