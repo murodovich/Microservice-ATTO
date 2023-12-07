4dcfd63b7b8da1d0c83d1e3ff30fb377e7cfe20c
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 15:40:15 +0500

    Create

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/UseCases/Teachers/Commands/CreateTeacherCommand.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+using School.Domain.Enums;
+
+namespace School.Application.UseCases.Teachers.Commands
+{
+    public class CreateTeacherCommand : IRequest<bool>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
