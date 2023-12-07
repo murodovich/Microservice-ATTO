773be7f1bc5a69f480ccb038c7a63b2100263311
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 14:27:53 +0500

    Student

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/UseCases/Students/Handler/GetByIdStudentQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using School.Application.Absreactions;
+using School.Application.UseCases.Students.Queries;
+using School.Domain.Entities.Students;
+using School.Domain.Exceptions.Students;
+
+namespace School.Application.UseCases.Students.Handler
+{
+    public class GetByIdStudentQueryHandler : IRequestHandler<GetByIdStudentQuery, Student>
+    {
+        private readonly ISchoolDbContext _dbContext;
+
+        public GetByIdStudentQueryHandler(ISchoolDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Student> Handle(GetByIdStudentQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.students.FirstOrDefaultAsync(x => x.StudentId == request.Id);
+            if (result == null) throw new StudentNotFoundException();
+            return result;
+        }
+    }
+}
