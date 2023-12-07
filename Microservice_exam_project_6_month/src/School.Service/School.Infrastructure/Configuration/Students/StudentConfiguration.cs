226197587959a06584b4f1fc07a6cc38e2220275
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 13:31:22 +0500

    Studendt

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Infrastructure/Configuration/Students/StudentConfiguration.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using School.Domain.Entities.Students;
+
+namespace School.Infrastructure.Configuration.Students
+{
+    public class StudentConfiguration : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder.HasKey(s => s.StudentId);
+
+            builder.Property(s => s.FirstName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(s => s.LastName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(s => s.UserName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(s => s.Password)
+                .IsRequired()
+                .HasMaxLength(255); 
+
+            builder.Property(s => s.Email)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.Property(s => s.Age)
+                .IsRequired();
+
+            builder.Property(s => s.BirthDate)
+                .IsRequired();
+
+
+            builder.HasMany(s => s.StudentCourses)
+                .WithOne(sc => sc.Student)
+                .HasForeignKey(sc => sc.StudentId);
+
+            builder.HasIndex(s => s.UserName).IsUnique();
+            builder.HasIndex(s => s.Email).IsUnique();
+        }
+    }
+}
