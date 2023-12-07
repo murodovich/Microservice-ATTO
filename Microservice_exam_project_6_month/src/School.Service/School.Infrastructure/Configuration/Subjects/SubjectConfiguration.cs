248f1453a926a25183f7d3a5a26c4d19428f21dc
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 13:31:29 +0500

    Subject

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Infrastructure/Configuration/Subjects/SubjectConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using School.Domain.Entities.Subjects;
+
+namespace School.Infrastructure.Configuration.Subjects
+{
+    public class SubjectConfiguration : IEntityTypeConfiguration<Subject>
+    {
+        public void Configure(EntityTypeBuilder<Subject> builder)
+        {
+            builder.HasKey(s => s.SubjectId);
+
+            builder.Property(s => s.Name)
+                .IsRequired()
+                .HasMaxLength(255);
+
+            builder.HasMany(s => s.Courses)
+                .WithOne(c => c.Subject)
+                .HasForeignKey(c => c.SubjectId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            builder.HasIndex(s => s.Name).IsUnique();
+        }
+    }
+}
