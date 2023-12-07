cf5f0e9f73e462b9a86d33c98cefd6c24aefba0f
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-07 10:34:49 +0500

    Teacher

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Domain/Entities/Teachers/Teacher.cs
@@ -0,0 +1,13 @@
+﻿using School.Domain.Entities.Courses;
+
+namespace School.Domain.Entities.Teachers
+{
+    public class Teacher
+    {
+        public int TeacherId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public ICollection<Course> Courses { get; set; }
+    }
+}
