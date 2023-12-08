493b784d5a078b3c6c7195adad72b376cb46fff8
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-08 17:00:41 +0500

    Program

--- Microservice_exam_project_6_month/src/Microservice.Getaway/Microservice.Getaway.Api/Program.cs
+++ Microservice_exam_project_6_month/src/Microservice.Getaway/Microservice.Getaway.Api/Program.cs
@@ -1,25 +1,29 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddOcelot();
+builder.Configuration.AddJsonFile("ocelot.json");
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+
+
 
 app.UseAuthorization();
-
+app.UseOcelot();
 app.MapControllers();
 
 app.Run();
