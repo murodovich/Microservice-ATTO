80533b5d3aa5a3bea4954a463d131a64335d39eb
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-08 19:48:41 +0500

    dcsvds

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/UseCases/Courses/Handler/UpdateCourseCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using School.Application.Absreactions;
+using School.Application.UseCases.Courses.Commands;
+using School.Domain.Exceptions.Courses;
+
+namespace School.Application.UseCases.Courses.Handler
+{
+    public class UpdateCourseCommandHandler : IRequestHandler<UpdateCourseCommand, bool>
+    {
+        private readonly ISchoolDbContext _context;
+
+        public UpdateCourseCommandHandler(ISchoolDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Handle(UpdateCourseCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _context.courses.FirstOrDefaultAsync(x => x.CourseId == request.Id);
+            if (result == null) throw new CourseNotFoundException();
+
+            result.Name = request.Name;
+            result.TeacherId = request.TeacherId;
+            result.SubjectId = request.SubjectId;
+
+            _context.courses.Update(result);
+            var res = await _context.SaveChangesAsync(cancellationToken);
+            return res > 0;
+
+
+        }
+    }
+}
