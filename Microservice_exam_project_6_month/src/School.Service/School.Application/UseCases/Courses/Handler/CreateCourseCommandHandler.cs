16299033d771c5fe35d43c66bfde8d9680486f54
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-08 17:01:34 +0500

    CommandHandler

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/UseCases/Courses/Handler/CreateCourseCommandHandler.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using School.Application.UseCases.Courses.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace School.Application.UseCases.Courses.Handler
+{
+    public class CreateCourseCommandHandler : IRequestHandler<CreateCourseCommand, bool>
+    {
+        public Task<bool> Handle(CreateCourseCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
