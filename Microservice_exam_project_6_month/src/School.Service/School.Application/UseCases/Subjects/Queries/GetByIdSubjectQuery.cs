3f4c68979eedddc5e55af32181fcbab3ffa9267d
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-08 19:50:11 +0500

    cscdssdcsc

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/UseCases/Subjects/Queries/GetByIdSubjectQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using School.Domain.Entities.Subjects;
+
+namespace School.Application.UseCases.Subjects.Queries
+{
+    public class GetByIdSubjectQuery : IRequest<Subject>
+    {
+        public int Id { get; set; }
+    }
+}
