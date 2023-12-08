4034cb3c76b813ded90f0fbc6c5c6e1267c0ae14
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-08 19:49:26 +0500

    cscscd

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/UseCases/StudentCourses/Queries/GetAllStudentCourseQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using School.Domain.Entities.StudentCourses;
+
+namespace School.Application.UseCases.StudentCourses.Queries
+{
+    public class GetAllStudentCourseQuer : IRequest<List<StudentCourse>>
+    {
+    }
+}
