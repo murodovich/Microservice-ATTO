725b181aeee9f60492a39dbf2d8a5fbda05614af
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-08 19:47:39 +0500

    StudentCourseController

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Api/Controllers/StudentCourses/StudentCourseController.cs
@@ -0,0 +1,50 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using School.Application.UseCases.StudentCourses.Commands;
+using School.Application.UseCases.StudentCourses.Queries;
+
+namespace School.Api.Controllers.StudentCourses
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class StudentCourseController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public StudentCourseController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async ValueTask<IActionResult> GetAllStudentCourseSchool()
+        {
+            var result = await _mediator.Send(new GetAllStudentCourseQuer());
+            return Ok(result);
+        }
+        [HttpPost]
+        public async ValueTask<IActionResult> CreateStudentCourseSchool([FromForm]CreateStudentCourseCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Created");
+        }
+        [HttpGet]
+        public async ValueTask<IActionResult> GetByIdStudentCourseSchool(int id)
+        {
+            var result = await _mediator.Send(new GetByIdStudentCourseQuery() { Id = id });
+            return Ok(result);
+        }
+        [HttpPut]
+        public async ValueTask<IActionResult> UpdateStudentCourseSchool([FromForm]UpdateStudentCourseCommand command)
+        {
+            await _mediator.Send(command);
+            return Ok("Updated");
+        }
+        [HttpDelete]
+        public async ValueTask<IActionResult> DeleteStudentCourseSchool(int Id)
+        {
+            await _mediator.Send(new DeleteStudentCourseCommand() { Id = Id });
+            return Ok("Deleted");
+        }
+    }
+}
