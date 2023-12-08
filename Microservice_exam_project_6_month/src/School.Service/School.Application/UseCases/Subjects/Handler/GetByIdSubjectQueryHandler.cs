7ec44035d37fad606fa521fb68fd11561de46089
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-08 19:49:57 +0500

    cscs

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/UseCases/Subjects/Handler/GetByIdSubjectQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using School.Application.Absreactions;
+using School.Application.UseCases.Subjects.Queries;
+using School.Domain.Entities.Subjects;
+using School.Domain.Exceptions.Subjects;
+
+namespace School.Application.UseCases.Subjects.Handler
+{
+    public class GetByIdSubjectQueryHandler : IRequestHandler<GetByIdSubjectQuery, Subject>
+    {
+        private readonly ISchoolDbContext _context;
+
+        public GetByIdSubjectQueryHandler(ISchoolDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Subject> Handle(GetByIdSubjectQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _context.subjects.FirstOrDefaultAsync(x => x.SubjectId == request.Id);
+            if (result == null) throw new SubjectNotFoundException();
+            return result;
+        }
+    }
+}
