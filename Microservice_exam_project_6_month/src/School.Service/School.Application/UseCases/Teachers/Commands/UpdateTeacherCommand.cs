5d20820497669ff032af970916e5da014db55a3a
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-08 17:01:55 +0500

    UpdateTeacher

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/UseCases/Teachers/Commands/UpdateTeacherCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using School.Domain.Enums;
+
+namespace School.Application.UseCases.Teachers.Commands
+{
+    public class UpdateTeacherCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
