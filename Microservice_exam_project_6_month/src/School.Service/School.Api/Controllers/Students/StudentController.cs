93ed932a5ce67fd24a5de44c4a03e20297af791b
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-08 17:00:48 +0500

    Student

--- Microservice_exam_project_6_month/src/School.Service/School.Api/Controllers/Students/StudentController.cs
+++ Microservice_exam_project_6_month/src/School.Service/School.Api/Controllers/Students/StudentController.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using School.Application.UseCases.Students.Commands;
 using School.Application.UseCases.Students.Queries;
 
@@ -10,21 +11,31 @@ namespace School.Api.Controllers.Students
     public class StudentController : ControllerBase
     {
         private readonly IMediator _mediator;
+        private readonly IMemoryCache _memoryCache;
 
-        public StudentController(IMediator mediator)
+        public StudentController(IMediator mediator, IMemoryCache memoryCache)
         {
             _mediator = mediator;
+            _memoryCache = memoryCache;
         }
         [HttpGet]
         public async ValueTask<IActionResult> GetAllStudentSchool()
         {
-            var result = await _mediator.Send(new GetAllStudentQuery());
-            return Ok(result);
+            var casheddata = _memoryCache.Get("GetAll");
+            if(casheddata == null)
+            {
+               var result =  await _mediator.Send(new GetAllStudentQuery());
+                _memoryCache.Set("GetAll",result);
+
+            }
+          
+            return Ok( _memoryCache.Get("GetAll"));
         }
 
         [HttpPost]
-        public async ValueTask<IActionResult> CreateStudentSchool(CreateStudentCommand command)
+        public async ValueTask<IActionResult> CreateStudentSchool([FromForm]CreateStudentCommand command)
         {
+            _memoryCache.Remove("GetAll");
             await _mediator.Send(command);
             return Ok("Created");
         }
@@ -32,11 +43,16 @@ namespace School.Api.Controllers.Students
         [HttpGet]
         public async ValueTask<IActionResult> GetByIdStudentSchool(int id)
         {
-            var result = await _mediator.Send(new GetByIdStudentQuery() { Id = id });
-            return Ok(result);
+            var res  = _memoryCache.Get(id);
+            if (res == null)
+            {
+                var result = await _mediator.Send(new GetByIdStudentQuery() { Id = id });
+                var res1 = _memoryCache.Set(id, result);
+            }
+            return Ok(_memoryCache.Get(id));
         }
         [HttpPut]
-        public async ValueTask<IActionResult> UpdateStudentSchool(UpdateStudentCommand command)
+        public async ValueTask<IActionResult> UpdateStudentSchool([FromForm] UpdateStudentCommand command)
         {
             await _mediator.Send(command);
             return Ok("Updated");
