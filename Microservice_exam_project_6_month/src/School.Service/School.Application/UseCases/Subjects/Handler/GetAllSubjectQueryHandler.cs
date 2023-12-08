8fa097be12da9dc19431053a11884dff44221492
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-08 19:49:53 +0500

    cscdsd

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/UseCases/Subjects/Handler/GetAllSubjectQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using School.Application.Absreactions;
+using School.Application.UseCases.Subjects.Queries;
+using School.Domain.Entities.Subjects;
+
+namespace School.Application.UseCases.Subjects.Handler
+{
+    public class GetAllSubjectQueryHandler : IRequestHandler<GetAllSubjectQuery, List<Subject>>
+    {
+        private readonly ISchoolDbContext _context;
+
+        public GetAllSubjectQueryHandler(ISchoolDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Subject>> Handle(GetAllSubjectQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _context.subjects.ToListAsync();
+            return result;
+        }
+    }
+}
