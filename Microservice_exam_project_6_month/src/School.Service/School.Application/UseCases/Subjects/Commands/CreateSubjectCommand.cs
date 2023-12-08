8a2e9a47a58d0c5fd29e19487aaf6c9b868a5f8c
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-08 19:49:34 +0500

    cscscd

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/UseCases/Subjects/Commands/CreateSubjectCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace School.Application.UseCases.Subjects.Commands
+{
+    public class CreateSubjectCommand : IRequest<bool>
+    {
+        public string Name { get; set; }
+    }
+}
