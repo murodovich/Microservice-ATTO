a047bd5acc57bf6738bb383f0f4dead09943a60c
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-08 19:48:16 +0500

    UpdateCourseController

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/UseCases/Courses/Commands/UpdateCourseCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+
+namespace School.Application.UseCases.Courses.Commands
+{
+    public class UpdateCourseCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int SubjectId { get; set; }
+        public int TeacherId { get; set; }
+    }
+}
