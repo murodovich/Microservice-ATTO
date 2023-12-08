c85f452e06a37ce909eab99f22f714093e22d113
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-08 19:49:42 +0500

    cscdscd

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/UseCases/Subjects/Commands/UpdateSubjectCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace School.Application.UseCases.Subjects.Commands
+{
+    public class UpdateSubjectCommand : IRequest<bool>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
