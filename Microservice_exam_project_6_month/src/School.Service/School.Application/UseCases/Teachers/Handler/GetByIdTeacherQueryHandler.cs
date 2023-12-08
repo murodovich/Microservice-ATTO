5246ed26b251b703bf15fc8ccaf082b65ee3b758
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-08 17:02:22 +0500

    GetById

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/UseCases/Teachers/Handler/GetByIdTeacherQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using School.Application.Absreactions;
+using School.Application.UseCases.Teachers.Queries;
+using School.Domain.Entities.Teachers;
+using School.Domain.Exceptions.Teachers;
+
+namespace School.Application.UseCases.Teachers.Handler
+{
+    public class GetByIdTeacherQueryHandler : IRequestHandler<GetByIdTeacherQuery, Teacher>
+    {
+        private readonly ISchoolDbContext _context;
+
+        public GetByIdTeacherQueryHandler(ISchoolDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Teacher> Handle(GetByIdTeacherQuery request, CancellationToken cancellationToken)
+        {
+            var result = await _context.teachers.FirstOrDefaultAsync(x => x.TeacherId == request.Id);
+            if (result == null) throw new TeacherNotFoundException();
+            return result;
+        }
+    }
+}
