cec299f14c5340d1963d54361bc8dd62b3be45e8
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-08 19:49:45 +0500

    cscsc

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/UseCases/Subjects/Handler/CreateSubjectCommandHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using School.Application.Absreactions;
+using School.Application.UseCases.Subjects.Commands;
+using School.Domain.Entities.Subjects;
+
+namespace School.Application.UseCases.Subjects.Handler
+{
+    public class CreateSubjectCommandHandler : IRequestHandler<CreateSubjectCommand, bool>
+    {
+        private readonly ISchoolDbContext _context;
+
+        public CreateSubjectCommandHandler(ISchoolDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Handle(CreateSubjectCommand request, CancellationToken cancellationToken)
+        {
+            var result = new Subject()
+            {
+                Name = request.Name,
+            };
+            await _context.subjects.AddAsync(result);
+
+            var res = await _context.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
+    }
+}
