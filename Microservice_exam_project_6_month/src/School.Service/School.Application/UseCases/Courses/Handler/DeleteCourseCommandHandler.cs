ad2e32bbb1513ec76891c0752d25a5b0785fc6a3
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-08 19:48:27 +0500

    DeleteCourseController

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/UseCases/Courses/Handler/DeleteCourseCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using School.Application.Absreactions;
+using School.Application.UseCases.Courses.Commands;
+using School.Domain.Exceptions.Courses;
+
+namespace School.Application.UseCases.Courses.Handler
+{
+    public class DeleteCourseCommandHandler : IRequestHandler<DeleteCourseCommand, bool>
+    {
+        private readonly ISchoolDbContext _dbContext;
+
+        public DeleteCourseCommandHandler(ISchoolDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<bool> Handle(DeleteCourseCommand request, CancellationToken cancellationToken)
+        {
+            var result = await _dbContext.courses.FirstOrDefaultAsync(x => x.CourseId == request.id);
+            if (result == null) throw new CourseNotFoundException();
+
+            _dbContext.courses.Remove(result);
+
+            var res = await _dbContext.SaveChangesAsync(cancellationToken);
+            return res > 0;
+
+        }
+    }
+}
