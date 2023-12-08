2e3eb374dc93cac847a4dbf1d9b84d7b6015cba3
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-08 19:49:06 +0500

    cscscs

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/UseCases/StudentCourses/Handler/CreateStudentCourseCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using School.Application.Absreactions;
+using School.Application.UseCases.StudentCourses.Commands;
+using School.Domain.Entities.StudentCourses;
+
+namespace School.Application.UseCases.StudentCourses.Handler
+{
+    public class CreateStudentCourseCommandHandler : IRequestHandler<CreateStudentCourseCommand, bool>
+    {
+        private readonly ISchoolDbContext _context;
+
+        public CreateStudentCourseCommandHandler(ISchoolDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> Handle(CreateStudentCourseCommand request, CancellationToken cancellationToken)
+        {
+            var result = new StudentCourse()
+            {
+                CourseId = request.CourseId,
+                StudentId = request.StudentId,
+                Grade = request.Grade,
+            };
+            await _context.studentsCourse.AddAsync(result);
+            var res = await _context.SaveChangesAsync(cancellationToken);
+            return res > 0;
+        }
+    }
+}
