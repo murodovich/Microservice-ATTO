9774a8b75675fd3d54a97b78fd707c6679dbb9c7
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-08 19:48:55 +0500

    cscdsc

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/UseCases/StudentCourses/Commands/CreateStudentCourseCommand.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using School.Domain.Enums;
+
+namespace School.Application.UseCases.StudentCourses.Commands
+{
+    public class CreateStudentCourseCommand : IRequest<bool>
+    {
+        public int StudentId { get; set; }
+        public int CourseId { get; set; }
+        public Grade? Grade { get; set; }
+    }
+}
