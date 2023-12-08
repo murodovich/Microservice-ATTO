b11347458c2f2cb484a202c84547e79bb8d6543b
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-08 17:02:39 +0500

    GetById

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/UseCases/Teachers/Queries/GetByIdTeacherQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using School.Domain.Entities.Teachers;
+
+namespace School.Application.UseCases.Teachers.Queries
+{
+    public class GetByIdTeacherQuery : IRequest<Teacher>
+    {
+        public int Id { get; set; }
+    }
+}
