d2b1321e0405981ff087f9aa2b0d235ac97cdc62
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-08 17:01:44 +0500

    DeleteCOmmand

---
+++ Microservice_exam_project_6_month/src/School.Service/School.Application/UseCases/Teachers/Commands/DeleteTeacherCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace School.Application.UseCases.Teachers.Commands
+{
+    public class DeleteTeacherCommand : IRequest<bool>
+    {
+        public int Id { get; set; } 
+    }
+}
