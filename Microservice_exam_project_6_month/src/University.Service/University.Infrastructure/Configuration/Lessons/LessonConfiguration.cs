c1dd5ae1a5f1b723e34e4df3cd23ed24002de962
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-09 22:19:40 +0500

    Lesson

--- Microservice_exam_project_6_month/src/University.Service/University.Infrastructure/Configuration/Lessons/LessonConfiguration.cs
+++ Microservice_exam_project_6_month/src/University.Service/University.Infrastructure/Configuration/Lessons/LessonConfiguration.cs
@@ -8,16 +8,16 @@ namespace University.Infrastructure.Configuration.Lessons
     {
         public void Configure(EntityTypeBuilder<Lesson> builder)
         {
-            builder.ToTable("Lessons"); 
+            builder.ToTable("Lessons");
 
-            builder.HasKey(l => l.LessonId); 
+            builder.HasKey(l => l.LessonId);
 
             builder.Property(l => l.CourseId)
                 .IsRequired();
 
             builder.Property(l => l.LessonName)
                 .IsRequired()
-                .HasMaxLength(255); 
+                .HasMaxLength(255);
 
             builder.Property(l => l.Date)
                 .IsRequired();
