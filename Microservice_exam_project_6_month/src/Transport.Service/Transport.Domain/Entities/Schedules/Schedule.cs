563405230a371db5426a860593c55d7e2f0a1937
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-09 22:25:29 +0500

    Scheldule

--- Microservice_exam_project_6_month/src/Transport.Service/Transport.Domain/Entities/Schedules/Schedule.cs
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Domain/Entities/Schedules/Schedule.cs
@@ -1,12 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using Transport.Domain.Entities.Routeis;
-
 namespace Transport.Domain.Entities.Schedules
 {
     public class Schedule
     {
         public int Id { get; set; }
-        public DateTime DepartureTime {  get; set; }
+        public DateTime DepartureTime { get; set; }
         public int TransportId { get; set; }
 
         [ForeignKey(nameof(Route))]
