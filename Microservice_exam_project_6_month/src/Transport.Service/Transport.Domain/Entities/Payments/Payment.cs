80c0ace59a5c5e729ba721ea72dc982418f96590
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-09 22:25:17 +0500

    Payments

--- Microservice_exam_project_6_month/src/Transport.Service/Transport.Domain/Entities/Payments/Payment.cs
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Domain/Entities/Payments/Payment.cs
@@ -1,12 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using Transport.Domain.Entities.Users;
-
 namespace Transport.Domain.Entities.Payments
 {
     public class Payment
     {
         public int Id { get; set; }
-        public decimal Amaunt {  get; set; }
+        public decimal Amaunt { get; set; }
         public DateTime PaymentDate { get; set; }
         [ForeignKey(nameof(Domain.Entities.Transports.Transport))]
         public int TransportId { get; set; }
