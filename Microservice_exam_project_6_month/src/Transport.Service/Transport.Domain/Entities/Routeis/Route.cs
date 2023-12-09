2c3b6b791638ea29963a756fed400cb9156b9ffd
Sarvar ibn Murod <123171146+murodovich@users.noreply.github.com>
2023-12-09 22:25:23 +0500

    Route

--- Microservice_exam_project_6_month/src/Transport.Service/Transport.Domain/Entities/Routeis/Route.cs
+++ Microservice_exam_project_6_month/src/Transport.Service/Transport.Domain/Entities/Routeis/Route.cs
@@ -1,10 +1,9 @@
 ﻿using Transport.Domain.Entities.Schedules;
-
 namespace Transport.Domain.Entities.Routeis
 {
     public class Route
     {
-        public  int Id { get; set; }
+        public int Id { get; set; }
         public string RouteName { get; set; }
         public string StartLocation { get; set; }
         public string EndLocation { get; set; }
